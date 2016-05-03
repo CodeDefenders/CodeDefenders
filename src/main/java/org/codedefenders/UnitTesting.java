f5de88e9451c822670788343bb6bf4036996ff9b
Jose Miguel Rojas <jmrs84@gmail.com>
2016-05-03 18:58:36 +0100

    utesting game mode

---
+++ src/main/java/org/codedefenders/UnitTesting.java
@@ -0,0 +1,220 @@
+package org.codedefenders;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseException;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.DoStmt;
+import com.github.javaparser.ast.stmt.ForStmt;
+import com.github.javaparser.ast.stmt.ForeachStmt;
+import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.ast.stmt.WhileStmt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.codedefenders.Constants.FILE_SEPARATOR;
+import static org.codedefenders.Constants.JAVA_SOURCE_EXT;
+import static org.codedefenders.Constants.SESSION_ATTRIBUTE_PREVIOUS_TEST;
+import static org.codedefenders.Constants.TESTS_DIR;
+import static org.codedefenders.Constants.TEST_DID_NOT_COMPILE_MESSAGE;
+import static org.codedefenders.Constants.TEST_DID_NOT_PASS_ON_CUT_MESSAGE;
+import static org.codedefenders.Constants.TEST_INVALID_MESSAGE;
+import static org.codedefenders.Constants.TEST_PASSED_ON_CUT_MESSAGE;
+import static org.codedefenders.Constants.TEST_PREFIX;
+
+public class UnitTesting extends HttpServlet {
+
+	private static final Logger logger = LoggerFactory.getLogger(UnitTesting.class);
+
+	// Based on info provided, navigate to the correct view for the user
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		// Get the session information specific to the current user.
+		HttpSession session = request.getSession();
+		int uid = (Integer) session.getAttribute("uid");
+
+		Game uTestingSession = DatabaseAccess.getActiveUnitTestingSession(uid);
+		session.setAttribute("uTestingSession", uTestingSession);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(Constants.UTESTING_VIEW_JSP);
+		dispatcher.forward(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		logger.debug("Executing doPost");
+
+		ArrayList<String> messages = new ArrayList<>();
+		HttpSession session = request.getSession();
+		int uid = (Integer) session.getAttribute("uid");
+
+		session.setAttribute("messages", messages);
+
+		Game uTestingSession = (Game) session.getAttribute("uTestingSession");
+
+		// Get the text submitted by the user.
+		String testText = request.getParameter("test");
+
+		// If it can be written to file and compiled, end turn. Otherwise, dont.
+		Test newTest = createTest(uTestingSession.getId(), uTestingSession.getClassId(), testText, uid);
+		if (newTest == null) {
+			messages.add(TEST_INVALID_MESSAGE);
+			session.setAttribute(SESSION_ATTRIBUTE_PREVIOUS_TEST, testText);
+			response.sendRedirect("utesting");
+			return;
+		}
+		System.out.println("New Test " + newTest.getId());
+		TargetExecution compileTestTarget = DatabaseAccess.getTargetExecutionForTest(newTest, TargetExecution.Target.COMPILE_TEST);
+
+		if (compileTestTarget.status.equals("SUCCESS")) {
+			TargetExecution testOriginalTarget = DatabaseAccess.getTargetExecutionForTest(newTest, TargetExecution.Target.TEST_ORIGINAL);
+			if (testOriginalTarget.status.equals("SUCCESS")) {
+				messages.add(TEST_PASSED_ON_CUT_MESSAGE);
+				uTestingSession.endRound();
+				uTestingSession.update();
+			} else {
+				// testOriginalTarget.status.equals("FAIL") || testOriginalTarget.status.equals("ERROR")
+				messages.add(TEST_DID_NOT_PASS_ON_CUT_MESSAGE);
+				messages.add(testOriginalTarget.message);
+				session.setAttribute(SESSION_ATTRIBUTE_PREVIOUS_TEST, testText);
+			}
+		} else {
+			messages.add(TEST_DID_NOT_COMPILE_MESSAGE);
+			messages.add(compileTestTarget.message);
+			session.setAttribute(SESSION_ATTRIBUTE_PREVIOUS_TEST, testText);
+		}
+		response.sendRedirect("utesting");
+	}
+
+	public File getNextSubDir(String path) {
+		File folder = new File(path);
+		folder.mkdirs();
+		String[] directories = folder.list(new FilenameFilter() {
+			@Override
+			public boolean accept(File current, String name) {
+				return new File(current, name).isDirectory() && (isParsable(name));
+			}
+		});
+		Arrays.sort(directories);
+		String newPath;
+		if (directories.length == 0)
+			newPath = folder.getAbsolutePath() + FILE_SEPARATOR + "1";
+		else {
+			File lastDir = new File(directories[directories.length - 1]);
+			int newIndex = Integer.parseInt(lastDir.getName()) + 1;
+			newPath = path + FILE_SEPARATOR + newIndex;
+		}
+		File newDir = new File(newPath);
+		newDir.mkdirs();
+		return newDir;
+	}
+
+	public static boolean isParsable(String input){
+		boolean parsable = true;
+		try{
+			Integer.parseInt(input);
+		}catch(NumberFormatException e){
+			parsable = false;
+		}
+		return parsable;
+	}
+
+	/**
+	 *
+	 * @param gid
+	 * @param cid
+	 * @param testText
+	 * @param ownerId
+	 * @return {@code null} if test is not valid
+	 * @throws IOException
+	 */
+	public Test createTest(int gid, int cid, String testText, int ownerId) throws IOException {
+
+		GameClass classUnderTest = DatabaseAccess.getClassForKey("Class_ID", cid);
+
+		File newTestDir = getNextSubDir(getServletContext().getRealPath(TESTS_DIR + FILE_SEPARATOR + gid));
+
+		String javaFile = createJavaFile(newTestDir, classUnderTest.getBaseName(), testText);
+
+		if (! validTestCode(javaFile)) {
+			return null;
+		}
+
+		// Check the test actually passes when applied to the original code.
+		Test newTest = AntRunner.compileTest(getServletContext(), newTestDir, javaFile, gid, classUnderTest, ownerId);
+		TargetExecution compileTestTarget = DatabaseAccess.getTargetExecutionForTest(newTest, TargetExecution.Target.COMPILE_TEST);
+
+		if (compileTestTarget != null && compileTestTarget.status.equals("SUCCESS")) {
+			AntRunner.testOriginal(getServletContext(), newTestDir, newTest);
+		}
+		return newTest;
+	}
+
+	private String createJavaFile(File dir, String classBaseName, String testCode) throws IOException {
+		String javaFile = dir.getAbsolutePath() + FILE_SEPARATOR + TEST_PREFIX + classBaseName + JAVA_SOURCE_EXT;
+		File testFile = new File(javaFile);
+		FileWriter testWriter = new FileWriter(testFile);
+		BufferedWriter bufferedTestWriter = new BufferedWriter(testWriter);
+		bufferedTestWriter.write(testCode);
+		bufferedTestWriter.close();
+		return javaFile;
+	}
+
+	private boolean validTestCode(String javaFile) throws IOException {
+
+		CompilationUnit cu;
+		FileInputStream in = null;
+		try {
+			in = new FileInputStream(javaFile);
+			// parse the file
+			cu = JavaParser.parse(in);
+			// prints the resulting compilation unit to default system output
+			if (cu.getTypes().size() != 1) {
+				System.out.println("Invalid test suite contains more than one type declaration.");
+				return false;
+			}
+			TypeDeclaration clazz = cu.getTypes().get(0);
+			if (clazz.getMembers().size() != 1) {
+				System.out.println("Invalid test suite contains more than one method.");
+				return false;
+			}
+			MethodDeclaration test = (MethodDeclaration)clazz.getMembers().get(0);
+			BlockStmt testBody = test.getBody();
+			for (Node node : testBody.getChildrenNodes()) {
+				if (node instanceof ForeachStmt
+						|| node instanceof IfStmt
+						|| node instanceof ForStmt
+						|| node instanceof WhileStmt
+						|| node instanceof DoStmt) {
+					System.out.println("Invalid test contains " + node.getClass().getSimpleName() + " statement");
+					return false;
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} finally {
+			in.close();
+		}
+		return true;
+	}
+}
---
+++ src/main/webapp/jsp/utesting_view.jsp
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<!-- Title -->
+	<title>Code Defenders - Defend</title>
+
+	<!-- App context -->
+	<base href="${pageContext.request.contextPath}/">
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js" type="text/javascript" ></script>
+
+	<!-- Slick -->
+	<link href="css/slick_1.5.9.css" rel="stylesheet" type="text/css" />
+	<script src="js/slick_1.5.9.min.js" type="text/javascript" ></script>
+
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js" type="text/javascript" ></script>
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+	<!-- Codemirror -->
+	<script src="codemirror/lib/codemirror.js" type="text/javascript" ></script>
+	<script src="codemirror/mode/javascript/javascript.js" type="text/javascript" ></script>
+	<script src="codemirror/mode/diff/diff.js" type="text/javascript" ></script>
+	<link href="codemirror/lib/codemirror.css" rel="stylesheet" type="text/css" />
+
+	<!-- Game -->
+	<link href="css/gamestyle.css" rel="stylesheet" type="text/css" />
+
+	<script>
+		$(document).ready(function() {
+			$('.single-item').slick({
+				arrows: true,
+				infinite: true,
+				speed: 300,
+				draggable:false
+			});
+			$('#messages-div').delay(10000).fadeOut();
+		});
+	</script>
+</head>
+
+<body>
+<%@ page import="java.util.*" %>
+<%@ page import="org.codedefenders.Test" %>
+<%@ page import="org.codedefenders.Game" %>
+<%@ page import="org.codedefenders.Constants" %>
+<%@ page import="static org.codedefenders.Game.State.ACTIVE" %>
+<% Game uTestingSession = (Game) session.getAttribute("uTestingSession"); %>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+			</button>
+			<a class="navbar-brand" href="/">
+				<span><img class="logo" href="/" src="images/logo.png"/></span>
+				Code Defenders
+			</a>
+		</div>
+		<div class= "collapse navbar-collapse" id="navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-left">
+				<li><a href="games/user">My Games</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li>
+					<p class="navbar-text">
+						<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+						<%=request.getSession().getAttribute("username")%>
+					</p>
+				</li>
+				<li><input type="submit" form="logout" class="btn btn-inverse navbar-btn" value="Log Out"/></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<form id="logout" action="login" method="post">
+	<input type="hidden" name="formType" value="logOut">
+</form>
+
+<%
+	ArrayList<String> messages = (ArrayList<String>) request.getSession().getAttribute("messages");
+	request.getSession().removeAttribute("messages");
+	if (messages != null && ! messages.isEmpty()) {
+%>
+<div class="alert alert-info" id="messages-div">
+	<% for (String m : messages) { %>
+	<pre><strong><%=m%></strong></pre>
+	<% } %>
+</div>
+<%	} %>
+
+<%
+	if (uTestingSession == null || uTestingSession.getState().equals(Game.State.FINISHED)) {
+%>
+<div class="row-fluid">
+	<h2> No unit testing session available for you at the moment. </h2>
+</div>
+<%  } else { %>
+
+<div class="row-fluid">
+	<div class="col-md-6" id="cut-div">
+		<h2>Class Under Test</h2>
+		<pre class="readonly-pre"><textarea class="readonly-textarea" id="sut" name="cut" cols="80" rows="30"><%=uTestingSession.getCUT().getAsString()%></textarea></pre>
+	</div> <!-- col-md6 left -->
+	<div class="col-md-6" id="utest-div">
+		<h2> Write a new JUnit test here
+			<% if (uTestingSession.getState().equals(ACTIVE)) {%>
+			<button type="submit" class="btn btn-primary btn-game btn-right" form="def">Submit</button>
+			<%}%>
+		</h2>
+		<form id="def" action="utesting" method="post">
+			<%
+				String testCode;
+				String previousTestCode = (String) request.getSession().getAttribute("previousTest");
+				request.getSession().removeAttribute("previousTest");
+				if (previousTestCode != null) {
+					testCode = previousTestCode;
+				} else
+					testCode = uTestingSession.getCUT().getTestTemplate();
+			%>
+			<pre><textarea id="code" name="test" cols="80" rows="30"><%= testCode %></textarea></pre>
+		</form>
+	</div> <!-- col-md6 right top -->
+</div> <!-- row-fluid 1 -->
+
+<div class="row-fluid">
+	<div class="col-md-6" id="submitted-div">
+		<h2> JUnit tests </h2>
+		<div class="slider single-item">
+			<%
+				boolean isTests = false;
+				for (Test t : uTestingSession.getExecutableTests()) {
+					isTests = true;
+					String tc = "";
+					for (String l : t.getHTMLReadout()) { tc += l + "\n"; }
+			%>
+			<div><h4>Test <%= t.getId() %></h4><pre class="readonly-pre"><textarea class="utest" cols="20" rows="10"><%=tc%></textarea></pre></div>
+			<%
+				}
+				if (!isTests) {%>
+			<div><h2></h2><p> There are currently no tests </p></div>
+			<%}
+			%>
+		</div> <!-- slider single-item -->
+	</div> <!-- col-md-6 left bottom -->
+</div>
+<%  } %>
+<script>
+	var editorTest = CodeMirror.fromTextArea(document.getElementById("code"), {
+		lineNumbers: true,
+		indentUnit: 4,
+		indentWithTabs: true,
+		matchBrackets: true
+	});
+	editorTest.on('beforeChange',function(cm,change) {
+		var text = cm.getValue();
+		var lines = text.split(/\r|\r\n|\n/);
+		var readOnlyLines = [0,1,2,3,4,5,6,7];
+		var readOnlyLinesEnd = [lines.length-1,lines.length-2];
+		if ( ~readOnlyLines.indexOf(change.from.line) || ~readOnlyLinesEnd.indexOf(change.to.line)) {
+			change.cancel();
+		}
+	});
+	editorTest.setSize("100%", 500);
+	var editorSUT = CodeMirror.fromTextArea(document.getElementById("sut"), {
+		lineNumbers: true,
+		matchBrackets: true,
+		readOnly: true
+	});
+	editorSUT.setSize("100%", 500);
+	/* Submitted tests */
+	var x = document.getElementsByClassName("utest");
+	var i;
+	for (i = 0; i < x.length; i++) {
+		CodeMirror.fromTextArea(x[i], {
+			lineNumbers: true,
+			matchBrackets: true,
+			readOnly: true
+		});
+	}
+</script>
+</body>
+</html>
