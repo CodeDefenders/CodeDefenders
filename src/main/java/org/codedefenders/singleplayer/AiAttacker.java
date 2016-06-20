32323f1702c16335f55d6185f035eeeb842018dc
Ben Clegg <bclegg@midcode.co.uk>
2016-06-20 16:30:48 +0100

    Added notes for implementation of ai turns to classes.

--- src/main/java/org/codedefenders/singleplayer/AiAttacker.java
+++ src/main/java/org/codedefenders/singleplayer/AiAttacker.java
@@ -6,8 +6,24 @@ import org.codedefenders.Game;
  * Created by midcode on 20/06/16.
  */
 public class AiAttacker extends AiPlayer {
+
 	public AiAttacker(Game g) {
 		super(g);
 		role = Game.Role.ATTACKER;
+	}
+
+	public void turnHard() {
+		//Use only one mutant per round.
+		//Perhaps modify the line with the least test coverage?
+	}
+
+	public void turnMedium() {
+		//Use one randomly selected mutant per round.
+	}
+
+	public void turnEasy() {
+		//Use multiple mutants per round, up to maximum amount.
+		int mutantsTotal = 1; //TODO: Find actual mutants total.
+		double maxNumMutants = Math.floor(mutantsTotal / game.getFinalRound());
 	}
 }
--- src/main/java/org/codedefenders/singleplayer/AiDefender.java
+++ src/main/java/org/codedefenders/singleplayer/AiDefender.java
@@ -4,6 +4,7 @@ import org.codedefenders.Game;
 import org.codedefenders.GameManager;
 import org.codedefenders.MutationTester;
 
+
 /**
  * Created by midcode on 20/06/16.
  */
@@ -22,5 +23,18 @@ public class AiDefender extends AiPlayer {
 		}
 		//Do nothing else, test is automatically re-run on new mutants by GameManager.
 		//TODO: Add equivalence check.
+		//Call equivalent only if test suite passes on mutant.
+	}
+
+	public void turnMedium() {
+		//Choose all tests which cover modified line(s)?
+		//Perhaps just 1 or 2?
+		//Perhaps higher chance of equivalence call? May happen due to weaker testing.
+	}
+
+	public void turnEasy() {
+		//Choose a random test which covers the modified line(s)?
+		//Perhaps just a random test?
+		//Perhaps higher chance of equivalence call? May happen due to weaker testing.
 	}
 }
