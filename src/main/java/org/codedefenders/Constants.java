dfad3dfe217d3a5728f6d474a666acf7bc43a254
Jose Miguel Rojas <jmrs84@gmail.com>
2016-08-10 20:32:18 +0100

    messages

--- src/main/java/org/codedefenders/Constants.java
+++ src/main/java/org/codedefenders/Constants.java
@@ -38,7 +38,7 @@ public class Constants {
 	public static final String TEST_INVALID_MESSAGE = "Your test is not valid. Remember the rules: Only one non-empty test, at most two assertions per test, no conditionals and no loops!";
 	public static final String TEST_PASSED_ON_CUT_MESSAGE = "Great! Your test compiled and passed on the original class under test.";
 	public static final String TEST_DID_NOT_PASS_ON_CUT_MESSAGE = "Your test did not pass on the original class under test. Try again.";
-	public static final String TEST_KILLED_CLAIMED_MUTANT_MESSAGE = "Yay, your test killed the allegedly equivalent mutant! You won the duel.";
+	public static final String TEST_KILLED_CLAIMED_MUTANT_MESSAGE = "Yay, your test killed the allegedly equivalent mutant. You won the duel!";
 	public static final String TEST_DID_NOT_KILL_CLAIMED_MUTANT_MESSAGE = "Oh no, your test did not kill the possibly equivalent mutant! You lost the duel.";
 	public static final String TEST_SUBMITTED_MESSAGE = "Test submitted and ready to kill mutants!";
 	public static final String TEST_KILLED_ZERO_MESSAGE = "Your test did not kill any mutant, just yet.";
@@ -47,12 +47,12 @@ public class Constants {
 	public static final String TEST_KILLED_N_MESSAGE = "Awesome! Your test killed %d mutants!"; // number of mutants
 
 	public static final String MUTANT_COMPILED_MESSAGE = "Your mutant was compiled successfully.";
-	public static final String MUTANT_ACCEPTED_EQUIVALENT_MESSAGE = "The mutant was marked equivalent.";
+	public static final String MUTANT_ACCEPTED_EQUIVALENT_MESSAGE = "The mutant was accepted as equivalent.";
 	public static final String MUTANT_UNCOMPILABLE_MESSAGE = "Your mutant failed to compile. Try again.";
 	public static final String MUTANT_INVALID_MESSAGE = "Sorry, your mutant is invalid: it is identical to the CUT or another mutant, or it contains invalid code (ifs, loops, or new logical ops.)";
 	public static final String MUTANT_CLAIMED_EQUIVALENT_MESSAGE = "Mutant claimed as equivalent, waiting for attacker to respond.";
 	public static final String MUTANT_CLAIMED_EQUIVALENT_ERROR_MESSAGE = "Something went wrong claiming equivalent mutant"; // TODO: How?
-	public static final String MUTANT_KILLED_BY_TEST_MESSAGE = "Test %d killed your mutant. Better luck in your next turn!"; // test
+	public static final String MUTANT_KILLED_BY_TEST_MESSAGE = "Test %d killed your mutant. Better luck with the next one!"; // test
 	public static final String MUTANT_SUBMITTED_MESSAGE = "Mutant submitted, may the force be with it.";
 	public static final String MUTANT_ALIVE_1_MESSAGE = "Cool, your mutant is alive.";
 	public static final String MUTANT_ALIVE_N_MESSAGE = "Awesome, your mutant survived %d existing tests!"; // number of tests
