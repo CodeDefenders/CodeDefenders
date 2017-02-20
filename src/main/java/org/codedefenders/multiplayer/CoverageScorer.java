dbe5c44d2bc387afce12f55b705905278530c87b
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2017-02-20 23:29:32 +0100

    Delete some dead code

--- src/main/java/org/codedefenders/multiplayer/CoverageScorer.java
+++ src/main/java/org/codedefenders/multiplayer/CoverageScorer.java
@@ -5,18 +5,19 @@ import org.codedefenders.Test;
 import org.codedefenders.scoring.Scorer;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by thoma on 27/06/2016.
  */
 public class CoverageScorer extends Scorer {
     @Override
-    protected int scoreTest(MultiplayerGame g, Test t, ArrayList<Mutant> killed) {
+    protected int scoreTest(MultiplayerGame g, Test t, List<Mutant> killed) {
         return (g.getDefenderValue() + killed.size()) * t.getLineCoverage().getLinesCovered().size();
     }
 
     @Override
-    protected int scoreMutant(MultiplayerGame g, Mutant m, ArrayList<Test> passed) {
+    protected int scoreMutant(MultiplayerGame g, Mutant m, List<Test> passed) {
         return (g.getAttackerValue() + passed.size());
     }
 }
--- src/main/java/org/codedefenders/multiplayer/LineMutant.java
+++
@@ -1,7 +0,0 @@
-package org.codedefenders.multiplayer;
-
-/**
- * Created by thoma on 03/06/2016.
- */
-public class LineMutant {
-}
