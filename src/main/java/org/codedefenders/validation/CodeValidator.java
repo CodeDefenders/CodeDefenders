705bac2ea1e61cc7e70b0b5c1efa55e3698a6120
wendli01 <wendling@fim.uni-passau.de>
2017-11-08 18:52:56 +0100

    Modified the Hash Generation so that comments are excluded.

--- src/main/java/org/codedefenders/validation/CodeValidator.java
+++ src/main/java/org/codedefenders/validation/CodeValidator.java
@@ -13,7 +13,9 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import static org.junit.Assert.assertTrue;
@@ -107,6 +109,31 @@ public class CodeValidator {
 	}
 
 	public static String getMD5(String code) {
-		return org.apache.commons.codec.digest.DigestUtils.md5Hex(code.replaceAll("\\s+",""));
+		String codeWithoutComments = getCodeWithoutComments(code);
+		return org.apache.commons.codec.digest.DigestUtils.md5Hex(codeWithoutComments);
+	}
+
+	/*
+Removes Comments (of both varieties) and Whitespaces from java source code.
+ */
+	private static String getCodeWithoutComments(String code) {
+		List<String> tokens = new ArrayList<>();
+		StreamTokenizer st = new StreamTokenizer(new StringReader(code));
+		st.slashSlashComments(true);
+		st.slashStarComments(true);
+		try {
+			while (st.nextToken() != StreamTokenizer.TT_EOF) {
+				if (st.ttype == StreamTokenizer.TT_NUMBER) {
+					tokens.add(String.valueOf(st.nval));
+				} else {
+					tokens.add(st.sval);
+				}
+			}
+		} catch (IOException e) {
+			// st.nextToken can throw an IOException if the stream ends
+			// As we have already parsed the code at this point it should
+			// be safe to assume this cannot happen (?)
+		}
+		return tokens.toString().replaceAll("\\s+", "");
 	}
 }
