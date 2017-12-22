eda44c89587d173ddbed6f4aeb76a86ef6cb2faf
Alessio Gambi <gambi@st.cs.uni-saarland.de>
2017-12-22 11:05:23 +0100

    Removed unused import

--- src/main/java/org/codedefenders/GameManager.java
+++ src/main/java/org/codedefenders/GameManager.java
@@ -9,8 +9,6 @@ import org.codedefenders.validation.CodeValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.mysql.fabric.xmlrpc.base.Data;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
