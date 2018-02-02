50d2ed6d25fee788a7cd3a064dd75dc20c806ba8
Alessio Gambi <gambi@st.cs.uni-saarland.de>
2018-02-02 12:32:19 +0100

    Mark by default method signatures and closing brackets of if statements without else branch.

--- src/test/java/org/codedefenders/CodeCoverageTest.java
+++ src/test/java/org/codedefenders/CodeCoverageTest.java
@@ -2,22 +2,23 @@ package org.codedefenders;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
+/**
+ * @author Alessio Gambi
+ */
 public class CodeCoverageTest {
-	
+
 	@Test
-	public void testTestCoverUnitializedFields() {
+	public void testTestCoverUnitializedFields() throws IOException {
 		// This class has 5 unitialized fields at lines: 12, 14, 16, 18, 20
-		GameClass gc = new GameClass("XmlElement", "XmlElement", "src/test/resources/itests/sources/XmlElement/XmlElement.java",
+		GameClass gc = new GameClass("XmlElement", "XmlElement",
+				"src/test/resources/itests/sources/XmlElement/XmlElement.java",
 				"src/test/resources/itests/sources/XmlElement/XmlElement.class");
 
-		// TODO We need to run the test
-		
-		// TODO We know that there are 5 non initialized fields from that class
-		assertEquals(5, gc.getLinesOfNonCoverableCode().size());
-		// TODO add assertions that check line number corresponds
-
+		assertEquals(5, gc.getLinesOfNonInitializedFields().size());
 	}
 
 	@Test
@@ -26,11 +27,38 @@ public class CodeCoverageTest {
 		GameClass gc = new GameClass("IntHashMap", "IntHashMap",
 				"src/test/resources/itests/sources/IntHashMap/IntHashMap.java", "");
 
-		// TODO We need to run the test
-
-		// TODO We know that there are 5 non initialized fields from that class
-		assertEquals(8, gc.getLinesOfNonCoverableCode().size());
-		// TODO add assertions that check line number corresponds
-
+		assertEquals(8, gc.getLinesOfNonInitializedFields().size());
 	}
+
+	@Test
+	public void testMethodSignatureAreUncoverableLines() {
+		// This class has 5 unitialized fields at lines: 12, 14, 16, 18, 20
+		GameClass gc = new GameClass("IntHashMap", "IntHashMap",
+				"src/test/resources/itests/sources/IntHashMap/IntHashMap.java", "");
+		System.out.println("CodeCoverageTest.testMethodSignatureAreUncoverableLines() " + gc.getLinesOfMethodSignatures());
+		// TODO Assert !
+//		assertEquals(8, gc.getLinesOfNonCoverableCode().size());
+	}
+	
+	@Test
+	public void testClosingBracketsAreUncoverableLines() {
+		// This class has 5 unitialized fields at lines: 12, 14, 16, 18, 20
+		GameClass gc = new GameClass("Document", "Document",
+				"src/test/resources/itests/sources/Document/Document.java", "");
+		System.out.println("CodeCoverageTest.testMethodSignatureAreUncoverableLines() " + gc.getUnreachableClosingBracketsForIfStatements() );
+		// TODO Assert !
+//		assertEquals(8, gc.getLinesOfNonCoverableCode().size());
+	}
+	
+	@Test
+	public void testSkipMethodSignatureForInterfaces() {
+		// This class has 5 unitialized fields at lines: 12, 14, 16, 18, 20
+		GameClass gc = new GameClass("Document", "Document",
+				"src/test/resources/itests/sources/Document/Document.java", "");
+		System.out.println("CodeCoverageTest.testMethodSignatureAreUncoverableLines() " + gc.getLinesOfMethodSignatures());
+	}
+	
+	
+	
+
 }
