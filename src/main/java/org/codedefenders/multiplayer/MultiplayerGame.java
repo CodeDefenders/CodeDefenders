da14f2a37cdb61a1ab4320ce48bd47ceb4213f70
wendli01 <wendling@fim.uni-passau.de>
2018-03-05 17:54:45 +0100

    Implemented marking uncovered on/off

--- src/main/java/org/codedefenders/multiplayer/MultiplayerGame.java
+++ src/main/java/org/codedefenders/multiplayer/MultiplayerGame.java
@@ -155,6 +155,10 @@ public class MultiplayerGame extends AbstractGame {
 		return maxAssertionsPerTest;
 	}
 
+	public boolean isMarkUncovered(){
+		return markUncovered;
+	}
+
 	/**
 	 * Get winning team. Return NONE if a draw.
 	 *
