e42987ea49e8be68a0ad5d481388d87b7e7b2ef7
Marvin Kreis <kreismar@fim.uni-passau.de>
2018-10-29 19:21:00 +0100

    Check for empty list of changed lines in mutant comparator

--- src/main/java/org/codedefenders/game/Mutant.java
+++ src/main/java/org/codedefenders/game/Mutant.java
@@ -625,7 +625,20 @@ public class Mutant implements Serializable {
 		return new Comparator<Mutant>() {
 			@Override
 			public int compare(Mutant o1, Mutant o2) {
-				return ((Integer) Collections.min( o1.getLines() ) ) - ((Integer) Collections.min( o2.getLines() ));
+				List<Integer> lines1 = o1.getLines();
+				List<Integer> lines2 = o2.getLines();
+
+				if (lines1.isEmpty()) {
+					if (lines2.isEmpty()) {
+						return 0;
+					} else {
+						return -1;
+					}
+				} else if (lines2.isEmpty()) {
+					return 1;
+				}
+
+				return Collections.min(lines1) - Collections.min(lines2);
 			}
 		};
 	}
