b58f518ebcbf5a4f6825d678475db0b3a51d9b91
Alessio Gambi <gambi@fim.uni-passau.de>
2018-11-08 14:50:23 +0100

    Mods to DB to enable intention collection

---
+++ scripts/1.3/add_intention.sql
@@ -0,0 +1,15 @@
+-- Add new table to store intentions
+DROP TABLE IF EXISTS `intention`;
+CREATE TABLE `intention` (
+  `Intention_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Test_ID` int(11) NOT NULL,
+  `Game_ID` int(11) NOT NULL,
+  `Target_Mutants` longtext,
+  `Target_Lines` longtext,
+   PRIMARY KEY (`Intention_ID`)
+) AUTO_INCREMENT=1
+
+-- Include per Game configuration
+ALTER TABLE games
+ADD COLUMN DeclareCoveredLines TINYINT(1) NOT NULL DEFAULT 0,
+ADD COLUMN DeclareKilledMutants TINYINT(1) NOT NULL DEFAULT 0;
