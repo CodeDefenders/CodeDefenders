e88754931837b0e46d7442b208b7387d1766ad97
Alessio Gambi <gambi@fim.uni-passau.de>
2018-11-20 17:23:44 +0100

    Failing test

---
+++ src/test/resources/itests/mutants/ClassWithPrivateInnerClass/ClassWithPrivateInnerClass.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ClassWithPrivateInnerClass {
+	private static class InnerClass {
+		int foo;
+
+		protected InnerClass(int x) {
+			foo = 0;
+		}
+	}
+
+	public int foo(int x) {
+		InnerClass ic = new InnerClass(x);
+		return ic.foo;
+	}
+}
