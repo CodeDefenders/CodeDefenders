bf91c40549307ec3a818b6caa242f9d4413fed1f
Phil Werli <werli@fim.uni-passau.de>
2019-03-17 22:11:02 +0100

    Update key length of test smell
    191 is the max length for utf8mb4 encoding.

--- src/main/resources/db/codedefenders.sql
+++ src/main/resources/db/codedefenders.sql
@@ -20,7 +20,7 @@ CREATE TABLE killmapjob
 DROP TABLE IF EXISTS `test_smell`;
 CREATE TABLE test_smell (
 	`Test_ID` int(11),
-	`smell_name` VARCHAR(500),
+	`smell_name` VARCHAR(191),
 	PRIMARY KEY (Test_ID, smell_name)
 );
 
