c50cafc42a61d8cb4ca057098255d408b6496f28
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2019-11-03 23:03:04 +0100

    Dynamically config for Tomcat Manager and CodeDefenders admin UI
    - Configures dynamically, based on environment variables, the allowed IP
    for Apache Tomcat Manager.
    - Users to access Tomcat Manager and CodeDefenders Administrative UI are
    configured dynamically too.

--- docker/README.md
+++ docker/README.md
@@ -1,8 +1,39 @@
 # Docker build instructions
-
-1. Create a ```CodeDefenders/docker/.env``` file that includes the following content
+1. Create compose network + containers but not starting them
+```
+docker-compose up --no-start
+```
+2. Next we are going to get the IP of the docker host for the ```docker_default``` network (docker host IP <=> gateway IP in docker_default network). Execute following command and copy its output:
+```
+docker network inspect docker_default --format '{{(index (index .IPAM.Config) 0).Gateway}}'
+```
+3. Create a ```CodeDefenders/docker/.env``` file that includes the following content
 ```
 # Replace this version number with the Codefender's version you want to install
 RELEASE_VERSION=v1.6.1
+# Replace with the calculated IP in step 2.
+ALLOWED_REMOTE_ADDR_REGEXP=172.XXX.YYY.ZZZ
 ```
-2. ```docker-compose up```
+2. Start the containers:
+```
+docker-compose up
+```
+
+Now you can access to CodeDefenders:
+- [Regular UI](http://localhost/codedefenders)
+- [Administrative UI](http://localhost/codedefenders/admin). Accessing the Admnistrative UI requires an additional user+password combination (check ```CODEDEF_MANAGER_USERNAME``` below).
+
+# Configuration options for CodeDefenders (```frontend``` service)
+
+| Variable                            | Defaul value | Meaning |
+| :---:                               |    :----:    | :---: |
+| CODEDEF_MANAGER_USERNAME            | user         | Username for the [Apache Tomcat manager](https://tomcat.apache.org/tomcat-9.0-doc/manager-howto.html#Configuring_Manager_Application_Access) |
+| CODEDEF_MANAGER_PASSWORD            | password     | Apache Tomcat manager user password |
+| CODEDEF_MANAGER_ROLES               | manager-gui  | Roles asigned to the Apache Tomcat manager user |
+| CODEDEF_MANAGER_ALLOWED_REMOTE_ADDR | 127.\d+.\d+.\d+ &#124; ::1 &#124; 0:0:0:0:0:0:0:1                                      | [Regular expression to check allowed IPs for Apache Tomcat manager](https://tomcat.apache.org/tomcat-9.0-doc/config/valve.html#Remote_Address_Valve) |
+| CODEDEF_ADMIN_USERNAME              | admin        | Username used to access CodeDefenders administrative interface |
+| CODEDEF_ADMIN_PASSWORD              | password     | CodeDefenders administrative user password |
+| CODEDEF_ADMIN_ROLES                 | manager-gui  | Roles asigned to the CodeDefenders administrative user |
+| CODEDEF_LOAD_BALANCER_IP            | IP of the "load-balancer" container | IP of the load-balancer use for CodeDefenders. This IP is configured as an [Apache Tomcat remote trusted proxy](https://tomcat.apache.org/tomcat-9.0-doc/config/valve.html#Remote_IP_Valve). |
+
+
---
+++ docker/frontend/docker-entrypoint.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+# Inspired from: https://github.com/docker-library/wordpress/blob/master/php7.1/apache/docker-entrypoint.sh
+set -euo pipefail
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+    local var="$1"
+    local fileVar="${var}_FILE"
+    local def="${2:-}"
+    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+        exit 1
+    fi
+    local val="$def"
+    if [ "${!var:-}" ]; then
+        val="${!var}"
+    elif [ "${!fileVar:-}" ]; then
+        val="$(< "${!fileVar}")"
+    fi
+    export "$var"="$val"
+    unset "$fileVar"
+}
+
+if [[ "$1" == catalina* ]]; then
+    envs=(
+        CODEDEF_MANAGER_USERNAME
+        CODEDEF_MANAGER_PASSWORD
+        CODEDEF_MANAGER_ROLES
+        CODEDEF_MANAGER_ALLOWED_REMOTE_ADDR
+        CODEDEF_ADMIN_USERNAME
+        CODEDEF_ADMIN_PASSWORD
+        CODEDEF_ADMIN_ROLES
+        CODEDEF_LOAD_BALANCER_IP
+    )
+    haveConfig=
+    for e in "${envs[@]}"; do
+        file_env "$e"
+        if [ -z "$haveConfig" ] && [ -n "${!e}" ]; then
+            haveConfig=1
+        fi
+    done
+
+    if [ "$haveConfig" ]; then
+        MAX_WAITS=10
+        get_loadbalancer_ip() {
+            waits=0
+            status="ko"
+            loadbalancer_ip=""
+            while [[ "$status" != "ok" ]]; do 
+                loadbalancer_ip=$(getent hosts load-balancer | cut -d ' ' -f1 ; test ${PIPESTATUS[0]} -eq 0)
+                if [ $? == 0 ]; then
+                    status="ok"
+                else
+                    sleep 1
+                fi
+                (( waits++ )) && (( waits == MAX_WAITS )) && break
+            done
+            if [[ "$status" != "ok" ]]; then
+                echo ""
+                exit 1
+            fi
+            echo $loadbalancer_ip
+            exit 0
+        }
+
+        : "${CODEDEF_MANAGER_USERNAME:=user}"
+        : "${CODEDEF_MANAGER_PASSWORD:=password}"
+        : "${CODEDEF_MANAGER_ROLES:=manager-gui}"
+        : "${CODEDEF_MANAGER_ALLOWED_REMOTE_ADDR:=127\\\\.\\\\d+\\\\.\\\\d+\\\\.\\\\d+|::1|0:0:0:0:0:0:0:1}"
+        : "${CODEDEF_ADMIN_USERNAME:=admin}"
+        : "${CODEDEF_ADMIN_PASSWORD:=password}"
+        : "${CODEDEF_ADMIN_ROLES:=manager-gui}"
+        : "${CODEDEF_LOAD_BALANCER_IP:=$(get_loadbalancer_ip)}"
+
+        add_tomcat_user() {
+            local username="$1"
+            local password="$2"
+            local roles="$3"
+            sed -i "/<\/tomcat-users>/i <user username=\"${username}\" password=\"${password}\" roles=\"${roles}\" />" /usr/local/tomcat/conf/tomcat-users.xml
+        }
+
+        add_remoteIp_valve() {
+            local trusted_proxy_ip="$1"
+            local context_file_path="$2"
+            sed -i "/<\/Context>/i<Valve className=\"org.apache.catalina.valves.RemoteIpValve\" remoteIpHeader=\"X-Real-IP\" internalProxies=\"\" trustedProxies=\"${trusted_proxy_ip}\" portHeader=\"X-Forwarded-Port\" hostHeader=\"X-Forwarded-Server\" />" "${context_file_path}"
+        }
+
+        add_remoteAddr_valve() {
+            local allowed_regexp="$1"
+            local context_file_path="$2"
+            sed -i "/<\/Context>/i<Valve className=\"org.apache.catalina.valves.RemoteAddrValve\" allow=\"${allowed_regexp}\" />" "${context_file_path}"
+        }
+
+        add_accessLog_valve() {
+            local host_name="$1"
+            local context_file_path="$2"
+            sed -i "/<\/Context>/i<Valve className=\"org.apache.catalina.valves.AccessLogValve\" directory=\"logs\" prefix=\"${host_name}_access_log\" suffix=\".txt\" requestAttributesEnabled=\"true\" pattern=\"%h %l %u %t &quot;%r&quot; %s %b\" />" "${context_file_path}"
+        }
+
+        cp "/usr/local/tomcat/templates/tomcat-users.xml" "/usr/local/tomcat/conf/tomcat-users.xml"
+        add_tomcat_user "${CODEDEF_MANAGER_USERNAME}" "${CODEDEF_MANAGER_PASSWORD}" "${CODEDEF_MANAGER_ROLES}"
+        add_tomcat_user "${CODEDEF_ADMIN_USERNAME}" "${CODEDEF_ADMIN_PASSWORD}" "${CODEDEF_ADMIN_ROLES}"
+
+        cp "/usr/local/tomcat/templates/manager-context.xml" "/usr/local/tomcat/webapps/manager/META-INF/context.xml"
+
+        if [[ "$CODEDEF_LOAD_BALANCER_IP" != "" ]]; then
+            add_remoteIp_valve "${CODEDEF_LOAD_BALANCER_IP}" "/usr/local/tomcat/webapps/manager/META-INF/context.xml"
+        fi
+        add_remoteAddr_valve "${CODEDEF_MANAGER_ALLOWED_REMOTE_ADDR}" "/usr/local/tomcat/webapps/manager/META-INF/context.xml"
+        add_accessLog_valve "manager" "/usr/local/tomcat/webapps/manager/META-INF/context.xml"
+
+        cp "/usr/local/tomcat/templates/codedefenders-context.xml" "/usr/local/tomcat/webapps/codedefenders/META-INF/context.xml"
+        if [[ "$CODEDEF_LOAD_BALANCER_IP" != "" ]]; then
+            add_remoteIp_valve "${CODEDEF_LOAD_BALANCER_IP}" "/usr/local/tomcat/webapps/codedefenders/META-INF/context.xml"
+        fi
+        add_accessLog_valve "codedefenders" "/usr/local/tomcat/webapps/codedefenders/META-INF/context.xml"
+    fi
+
+    # now that we're definitely done writing configuration, let's clear out the relevant environment variables (don't leak secrets)
+    for e in "${envs[@]}"; do
+        unset "$e"
+    done
+fi
+
+exec "$@"
