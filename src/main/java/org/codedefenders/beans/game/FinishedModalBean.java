2258b9276a373225331de684a14fcb51326e76ec
Marvin Kreis <kreismar@fim.uni-passau.de>
2020-01-14 06:44:49 +0100

    Add bean for finished_modal

---
+++ src/main/java/org/codedefenders/beans/game/FinishedModalBean.java
@@ -0,0 +1,41 @@
+package org.codedefenders.beans.game;
+
+import org.codedefenders.util.Constants;
+
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ * <p>Provides data for the finished modal game component.</p>
+ * <p>Bean Name: {@code finishedModal}</p>
+ */
+@ManagedBean
+@RequestScoped
+public class FinishedModalBean {
+    private GameOutcome outcome;
+
+    public FinishedModalBean() {
+        outcome = null;
+    }
+
+    // TODO: finished_modal is currently not used. Add setters as needed.
+
+    // --------------------------------------------------------------------------------
+
+    public String getMessage() {
+        switch (outcome) {
+            case WIN:
+                return Constants.WINNER_MESSAGE;
+            case LOSS:
+                return Constants.LOSER_MESSAGE;
+            case DRAW:
+                return Constants.DRAW_MESSAGE;
+            default:
+                return "";
+        }
+    }
+
+    private enum GameOutcome {
+        WIN, LOSS, DRAW
+    }
+}
