91faf4c8490ab7bb8799f19f28eec062ed45fb0d
Alexander Degenhart <degenhart@fim.uni-passau.de>
2020-06-01 14:59:44 +0200

    Move java version check to Configuration

--- src/main/java/org/codedefenders/SystemStartStop.java
+++ src/main/java/org/codedefenders/SystemStartStop.java
@@ -52,27 +52,21 @@ public class SystemStartStop implements ServletContextListener {
      */
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        logger.info("Java version: " + System.getProperty("java.version"));
-        if (getJavaMajorVersion() > 9) {
-            String errorMsg = "Unsupported java version! CodeDefenders needs at most Java 9";
-            logger.error(errorMsg);
-            throw new Error(errorMsg);
-        } else {
-            try {
-                ConnectionPool.instance();
-                logger.info("Code Defenders started successfully.");
-            } catch (Exception e) {
-                // Fail Deployment
-                throw new RuntimeException("Deployment failed. Reason: ", e);
-            }
-            mgr.register("test-executor").withMax(4).withCore(2).add();
-        }
         try {
             config.validate();
         } catch (ConfigurationValidationException e) {
             logger.error(e.getMessage());
             throw new RuntimeException("Invalid configuration! Reason: " + e.getMessage(), e);
         }
+        try {
+            ConnectionPool.instance();
+            logger.info("Code Defenders started successfully.");
+        } catch (Exception e) {
+            // Fail Deployment
+            throw new RuntimeException("Deployment failed. Reason: ", e);
+        }
+        mgr.register("test-executor").withMax(4).withCore(2).add();
+
     }
 
     /**
@@ -104,22 +98,5 @@ public class SystemStartStop implements ServletContextListener {
         }
 
         // The ThreadPoolManager should be able to automatically stop the instances
-    }
-
-    private static int getJavaMajorVersion() {
-        String version = System.getProperty("java.version");
-        if (version.startsWith("1.")) {
-            version = version.substring(2);
-        }
-        /* Allow these formats:
-         * 1.8.0_72-ea
-         * 9-ea
-         * 9
-         * 9.0.1
-         */
-        int dotPos = version.indexOf('.');
-        int dashPos = version.indexOf('-');
-        return Integer.parseInt(version.substring(0,
-                dotPos > -1 ? dotPos : dashPos > -1 ? dashPos : 1));
     }
 }
--- src/main/java/org/codedefenders/configuration/Configuration.java
+++ src/main/java/org/codedefenders/configuration/Configuration.java
@@ -91,6 +91,10 @@ public class Configuration {
                     + " is not a valid port number");
         }
 
+        if (getJavaMajorVersion() > 9) {
+            validationErrors.add("Unsupported java version! CodeDefenders needs at most Java 9");
+        }
+
         //if (clusterMode) {
         //    // TODO: Validate clusterOptions
         //}
@@ -149,6 +153,23 @@ public class Configuration {
 
     public boolean isMutantCoverage() {
         return mutantCoverage;
+    }
+
+    public int getJavaMajorVersion() {
+        String version = System.getProperty("java.version");
+        if (version.startsWith("1.")) {
+            version = version.substring(2);
+        }
+        /* Allow these formats:
+         * 1.8.0_72-ea
+         * 9-ea
+         * 9
+         * 9.0.1
+         */
+        int dotPos = version.indexOf('.');
+        int dashPos = version.indexOf('-');
+        return Integer.parseInt(version.substring(0,
+                dotPos > -1 ? dotPos : dashPos > -1 ? dashPos : 1));
     }
 
     @Override
