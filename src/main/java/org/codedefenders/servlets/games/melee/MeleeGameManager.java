05823129580be62a464586c02fb4c74f637567aa
Alexander Degenhart <degenhart@fim.uni-passau.de>
2020-06-16 12:11:56 +0200

    Don't allow different players to submit the same mutant because the database can't handle this

--- src/main/java/org/codedefenders/servlets/games/melee/MeleeGameManager.java
+++ src/main/java/org/codedefenders/servlets/games/melee/MeleeGameManager.java
@@ -584,8 +584,10 @@ public class MeleeGameManager extends HttpServlet {
         }
 
         Mutant existingMutant = gameManagingUtils.existingMutant(game.getId(), mutantText);
-        boolean duplicateCheckSuccess = existingMutant == null || existingMutant.getCreatorId() != playerId;
+        boolean duplicateCheckSuccess = existingMutant == null; //|| existingMutant.getPlayerId() != playerId;
         // TODO: Why allow duplicate mutants from different creators?
+        //  Currently not possible because of database constraint
+        //  See also: Issue #675
 
         MutantDuplicateCheckedEvent mdce = new MutantDuplicateCheckedEvent();
         mdce.setGameId(game.getId());
