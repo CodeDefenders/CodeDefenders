3c016a94fc8eb33c72171f60830259702d2483bc
Alexander Degenhart <degenhart@fim.uni-passau.de>
2020-07-18 10:56:49 +0200

    Merge TestAccordionDTO in TestAccordionBean

--- src/main/java/org/codedefenders/beans/game/TestAccordionBean.java
+++ src/main/java/org/codedefenders/beans/game/TestAccordionBean.java
@@ -1,19 +1,30 @@
 package org.codedefenders.beans.game;
 
+import com.google.common.collect.Range;
+import com.google.common.collect.RangeMap;
+import com.google.common.collect.TreeRangeMap;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.Expose;
+import org.codedefenders.dto.TestDTO;
 import org.codedefenders.game.AbstractGame;
-import org.codedefenders.game.TestAccordionDTO;
-import org.codedefenders.game.TestAccordionDTO.TestAccordionCategory;
+import org.codedefenders.game.GameClass;
+import org.codedefenders.game.Mutant;
+import org.codedefenders.game.Test;
 import org.codedefenders.util.JSONUtils;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * <p>Provides data for the test accordion game component.</p>
@@ -28,36 +39,102 @@ public class TestAccordionBean {
     AbstractGame game;
 
     /**
-     * Contains the test and mutant information to be displayed in the accordion.
+     * The categories of the test accordion, in sorted order.
+     * One category containing all tests and one category for each method of the class,
+     * containing the tests that cover the method.
      */
-    private TestAccordionDTO testAccordionData;
+    private List<TestAccordionCategory> categories;
 
-    public TestAccordionBean() {
-        testAccordionData = null;
-    }
+    /**
+     * Maps test ids to tests.
+     */
+    private Map<Integer, TestDTO> tests;
 
     @PostConstruct
     public void setup() {
-        testAccordionData = new TestAccordionDTO(game.getCUT(), game.getAllTests(), game.getMutants());
+        GameClass cut = game.getCUT();
+        List<Test> testsList = game.getAllTests();
+        List<Mutant> mutantsList = game.getMutants();
+
+        tests = new HashMap<>();
+        categories = new ArrayList<>();
+
+        for (Test test : testsList) {
+            tests.put(test.getId(), new TestDTO(test, mutantsList));
+        }
+
+        TestAccordionCategory allTests = new TestAccordionCategory("All Tests", "all");
+        allTests.addTestIds(tests.keySet());
+
+        List<GameClass.MethodDescription> methodDescriptions = cut.getMethodDescriptions();
+        List<TestAccordionCategory> methodCategories = new ArrayList<>();
+        for (int i = 0; i < methodDescriptions.size(); i++) {
+            methodCategories.add(new TestAccordionCategory(methodDescriptions.get(i), String.valueOf(i)));
+        }
+
+        categories.add(allTests);
+        categories.addAll(methodCategories);
+
+        if (methodCategories.isEmpty()) {
+            return;
+        }
+
+        /* Map ranges of methods to their test accordion infos. */
+        @SuppressWarnings("UnstableApiUsage")
+        RangeMap<Integer, TestAccordionCategory> methodRanges = TreeRangeMap.create();
+        for (TestAccordionCategory method : methodCategories) {
+            methodRanges.put(Range.closed(method.startLine, method.endLine), method);
+        }
+
+        Range<Integer> beforeFirst = Range.closedOpen(0, methodRanges.span().lowerEndpoint());
+
+        /* For every test, go through all covered lines and find the methods that are covered by it. */
+        for (Test test : testsList) {
+
+            /* Save the last range a line number fell into to avoid checking a line number in the same method twice. */
+            Range<Integer> lastRange = null;
+
+            for (Integer line : test.getLineCoverage().getLinesCovered()) {
+
+                /* Skip if line falls into a method that was already considered. */
+                if (lastRange != null && lastRange.contains(line)) {
+                    continue;
+                }
+
+                Map.Entry<Range<Integer>, TestAccordionCategory> entry = methodRanges.getEntry(line);
+
+                /* Line does not belong to any method. */
+                if (entry == null) {
+                    lastRange = beforeFirst;
+
+                    /* Line belongs to a method. */
+                } else {
+                    lastRange = entry.getKey();
+                    entry.getValue().addTestId(test.getId());
+                }
+            }
+        }
     }
 
     // --------------------------------------------------------------------------------
 
+    /**
+     * Returns the categories of the test accordion.
+     *
+     * @return The categories of the test accordion.
+     */
     public List<TestAccordionCategory> getCategories() {
-        return testAccordionData.getCategories();
+        return Collections.unmodifiableList(categories);
     }
 
-    public String getJSON() {
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(Map.class, new JSONUtils.MapSerializer())
-                .create();
-        return gson.toJson(testAccordionData);
+    public Map<Integer, TestDTO> getTests() {
+        return tests;
     }
 
     public String getCategoriesAsJSON() {
         Gson gson = new GsonBuilder()
                 .create();
-        return gson.toJson(testAccordionData.getCategories());
+        return gson.toJson(categories);
     }
 
     public String getTestsAsJSON() {
@@ -65,6 +142,53 @@ public class TestAccordionBean {
                 // It is important that its HashMap.class, it doesn't work if I change it to Map.class …
                 .registerTypeAdapter(HashMap.class, new JSONUtils.MapSerializer())
                 .create();
-        return gson.toJson(testAccordionData.getTests());
+        return gson.toJson(tests);
+    }
+
+
+    /**
+     * Represents a category (accordion section) of the test accordion.
+     */
+    public static class TestAccordionCategory {
+        @Expose
+        private String description;
+        private Integer startLine;
+        private Integer endLine;
+        @Expose
+        private Set<Integer> testIds;
+        @Expose
+        private String id;
+
+        public TestAccordionCategory(String description, String id) {
+            this.description = description;
+            this.testIds = new HashSet<>();
+            this.id = id;
+        }
+
+        public TestAccordionCategory(GameClass.MethodDescription methodDescription, String id) {
+            this(methodDescription.getDescription(), id);
+            this.startLine = methodDescription.getStartLine();
+            this.endLine = methodDescription.getEndLine();
+        }
+
+        public void addTestId(int testId) {
+            this.testIds.add(testId);
+        }
+
+        public void addTestIds(Collection<Integer> testIds) {
+            this.testIds.addAll(testIds);
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public Set<Integer> getTestIds() {
+            return Collections.unmodifiableSet(testIds);
+        }
+
+        public String getId() {
+            return id;
+        }
     }
 }
