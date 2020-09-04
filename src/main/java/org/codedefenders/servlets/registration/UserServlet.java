adc037517e5b9e676c31198b1f52eacddaa8144c
Alessio Gambi <alessio.gambi@uni-passau.de>
2020-09-04 09:21:57 +0200

    Refactored redirect to LOGIN_VIEW_JSP and introduced error respond for invalid requests

--- src/main/java/org/codedefenders/servlets/registration/UserServlet.java
+++ src/main/java/org/codedefenders/servlets/registration/UserServlet.java
@@ -3,8 +3,6 @@ package org.codedefenders.servlets.registration;
 import java.io.IOException;
 
 import javax.inject.Inject;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,10 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.codedefenders.beans.message.MessagesBean;
-import org.codedefenders.database.AdminDAO;
 import org.codedefenders.database.UserDAO;
 import org.codedefenders.model.User;
-import org.codedefenders.servlets.admin.AdminSystemSettings;
 import org.codedefenders.util.Constants;
 import org.codedefenders.util.Paths;
 import org.codedefenders.validation.input.CodeDefendersValidator;
@@ -48,49 +44,38 @@ public class UserServlet extends HttpServlet {
             if (!(validator.validUsername(username))) {
                 // This check should be performed in the user interface too.
                 messages.add("Could not create user. Invalid username.");
-                RequestDispatcher dispatcher = request.getRequestDispatcher(Constants.LOGIN_VIEW_JSP);
-                dispatcher.forward(request, response);
             } else if (!validator.validPassword(password)) {
                 // This check should be performed in the user interface too.
                 messages.add("Could not create user. Invalid password.");
-                RequestDispatcher dispatcher = request.getRequestDispatcher(Constants.LOGIN_VIEW_JSP);
-                dispatcher.forward(request, response);
             } else if (!validator.validEmailAddress(email)) {
                 // This check should be performed in the user interface too.
                 messages.add("Could not create user. Invalid E-Mail address.");
-                RequestDispatcher dispatcher = request.getRequestDispatcher(Constants.LOGIN_VIEW_JSP);
-                dispatcher.forward(request, response);
             } else if (!password.equals(confirm)) {
                 // This check should be performed in the user interface too.
                 messages.add("Could not create user. Password entries did not match.");
-                RequestDispatcher dispatcher = request.getRequestDispatcher(Constants.LOGIN_VIEW_JSP);
-                dispatcher.forward(request, response);
             } else if (UserDAO.getUserByName(username) != null) {
                 messages.add("Could not create user. Username is already taken.");
-                RequestDispatcher dispatcher = request.getRequestDispatcher(Constants.LOGIN_VIEW_JSP);
-                dispatcher.forward(request, response);
             } else if (UserDAO.getUserByEmail(email) != null) {
                 messages.add("Could not create user. Email has already been used. You can reset your password.");
-                RequestDispatcher dispatcher = request.getRequestDispatcher(Constants.LOGIN_VIEW_JSP);
-                dispatcher.forward(request, response);
             } else {
                 User newUser = new User(username, User.encodePassword(password), email);
                 if (newUser.insert()) {
                     messages.add("Your user has been created. You can login now.");
-                    RequestDispatcher dispatcher = request.getRequestDispatcher(Constants.LOGIN_VIEW_JSP);
-                    dispatcher.forward(request, response);
                 } else {
                     // TODO: How about some error handling?
                     messages.add("Could not create a user for you, sorry!");
-                    RequestDispatcher dispatcher = request.getRequestDispatcher(Constants.LOGIN_VIEW_JSP);
-                    dispatcher.forward(request, response);
                 }
             }
+            RequestDispatcher dispatcher = request.getRequestDispatcher(Constants.LOGIN_VIEW_JSP);
+            dispatcher.forward(request, response);
             break;
 
         default:
-            // ignored
+            // Anything different than "create" is an error, so we not allow it
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            break;
         }
+
     }
 
 }
