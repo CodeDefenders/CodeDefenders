f8952b9cef67e3e7290f9676d0f7b0e52870b84f
Marvin Kreis <kreismar@fim.uni-passau.de>
2020-11-05 16:13:05 +0100

    Add a bit of JavaDoc to GameChatBean

--- src/main/java/org/codedefenders/beans/game/GameChatBean.java
+++ src/main/java/org/codedefenders/beans/game/GameChatBean.java
@@ -15,6 +15,9 @@ import org.codedefenders.util.Paths;
 @RequestScoped
 @Named("gameChat")
 public class GameChatBean {
+    /**
+     * The maximum allowed message length.
+     */
     public static final int MAX_MESSAGE_LENGTH = 500;
 
     @Inject
@@ -24,14 +27,26 @@ public class GameChatBean {
     @Inject
     LoginBean login;
 
+    /**
+     * Checks whether the chat is enabled in the current game.
+     * @return Whether the chat is enabled.
+     */
     public boolean isChatEnabled() {
         return game.isChatEnabled();
     }
 
+    /**
+     * Returns the game id of the current game.
+     * @return The game id.
+     */
     public int getGameId() {
         return game.getId();
     }
 
+    /**
+     * Checks whether the all/attacker/defender tabs should be shown in the chat for the current game.
+     * @return Whether the tabs should be shown.
+     */
     public boolean isShowTabs() {
         if (game instanceof MultiplayerGame) {
             MultiplayerGame multiplayerGame = (MultiplayerGame) game;
@@ -42,10 +57,18 @@ public class GameChatBean {
         }
     }
 
+    /**
+     * Returns the URL for the chat API for the current game.
+     * @return The URL for the chat API.
+     */
     public String getChatApiUrl() {
         return Paths.API_GAME_CHAT.substring(1) + "?gameId=" + game.getId();
     }
 
+    /**
+     * Returns the maximum length for chat messages.
+     * @return The maximum length for chat messages.
+     */
     public int getMaxMessageLength() {
         return MAX_MESSAGE_LENGTH;
     }
