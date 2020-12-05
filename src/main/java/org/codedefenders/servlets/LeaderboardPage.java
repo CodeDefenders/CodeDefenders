f1c7663ab567fa40754af155deb4cb69f6382bc3
Alexander Degenhart <degenhart@fim.uni-passau.de>
2020-12-05 13:30:09 +0100

    Use JSTL for leaderboard

--- src/main/java/org/codedefenders/servlets/LeaderboardPage.java
+++ src/main/java/org/codedefenders/servlets/LeaderboardPage.java
@@ -27,11 +27,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.codedefenders.game.leaderboard.Leaderboard;
+
 @WebServlet("/leaderboard")
 public class LeaderboardPage extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setAttribute("leaderboardEntries", Leaderboard.getAll());
+
         req.getRequestDispatcher("/jsp/leaderboards.jsp").forward(req, resp);
     }
 }
