adbc102230faa864c92ddc6e5384b888425793f6
Alexander Degenhart <degenhart@fim.uni-passau.de>
2020-12-05 13:15:07 +0100

    Add servlets for melee/create and {melee,multiplayer}/history

---
+++ src/main/java/org/codedefenders/servlets/games/battleground/MultiplayerGameHistoryManager.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2020 Code Defenders contributors
+ *
+ * This file is part of Code Defenders.
+ *
+ * Code Defenders is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Code Defenders is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Code Defenders. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.codedefenders.servlets.games.battleground;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/multiplayer/history")
+public class MultiplayerGameHistoryManager extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/jsp/battleground/history_game_view.jsp").forward(req, resp);
+    }
+}
+
