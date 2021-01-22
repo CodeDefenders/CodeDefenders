3acbbb362b958ac81c1e59d6674454e38ee58e51
Alexander Degenhart <degenhart@fim.uni-passau.de>
2021-01-22 13:41:28 +0100

    Use JSTL for help page

--- src/main/java/org/codedefenders/servlets/HelpPage.java
+++ src/main/java/org/codedefenders/servlets/HelpPage.java
@@ -21,17 +21,26 @@ package org.codedefenders.servlets;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.codedefenders.beans.page.PageInfoBean;
+
 @WebServlet("/help")
 public class HelpPage extends HttpServlet {
 
+    @Inject
+    PageInfoBean pageInfo;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        pageInfo.setPageTitle("Help");
+
         req.getRequestDispatcher("/jsp/help.jsp").forward(req, resp);
     }
 }
