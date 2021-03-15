51a33ce8d41de2c5f7c7d965ed4b5b25290050ed
brunemarkus <none>
2021-03-15 11:33:59 +0100

    Adjust puzzle 3.2

--- installation/puzzles/mutants/Dataflow2/1/Puzzle.java
+++ installation/puzzles/mutants/Dataflow2/1/Puzzle.java
@@ -1,13 +1,18 @@
 public class Puzzle {
-  
+
   public int testMe(int x, int y) {
-    int z = 0;
-    if(x == 42) {
-      z = 4;
-    }
-    if(y == 100) {
-      z = 2;
-    }
-    return z;
-  }
+      public int testMe(int x, int y) {
+
+          int z = 0;
+
+          if(x == 42) {
+              z = 4;
+          }
+
+          if(y == 100) {
+              z = 2;
+          }
+
+          return z;
+      }
 }
--- installation/puzzles/mutants/Dataflow2/2/Puzzle.java
+++ installation/puzzles/mutants/Dataflow2/2/Puzzle.java
@@ -1,13 +1,18 @@
 public class Puzzle {
-  
+
   public int testMe(int x, int y) {
-    int z = 0;
-    if(x == 42) {
-      z = 1;
-    }
-    if(y == 100) {
-      z = 5;
-    }
-    return z;
-  }
+      public int testMe(int x, int y) {
+
+          int z = 0;
+
+          if(x == 42) {
+              z = 1;
+          }
+
+          if(y == 100) {
+              z = 5;
+          }
+
+          return z;
+      }
 }
