943cd3f65a72af954c8fc4644571596578fd6fbc
Alexander Degenhart <degenhart@fim.uni-passau.de>
2021-03-22 17:22:06 +0100

    Update migration script to consider GAME_PLAYER_LEFT events

--- src/main/resources/db/migrations/V9__fix_player_switch_side_events_not_deleted.sql
+++ src/main/resources/db/migrations/V9__fix_player_switch_side_events_not_deleted.sql
@@ -12,9 +12,15 @@ WHERE e.Event_ID IN (
                    AND Event_Status = 'GAME'
                  GROUP BY Game_ID, Player_ID
                  UNION
-                 SELECT MAX(Event_ID), Game_ID, Player_ID
+                 SELECT MAX(Event_ID) AS Event_ID, Game_ID, Player_ID
                  FROM events
                  WHERE (Event_Type = 'ATTACKER_JOINED' OR Event_Type = 'DEFENDER_JOINED')
+                   AND Event_Status = 'NEW'
+                 GROUP BY Game_ID, Player_ID
+                 UNION
+                 SELECT MAX(Event_ID) AS Event_ID, Game_ID, Player_ID
+                 FROM events
+                 WHERE (Event_Type = 'GAME_PLAYER_LEFT')
                    AND Event_Status = 'NEW'
                  GROUP BY Game_ID, Player_ID
              ) as FIRSTJOINEDEVENT
@@ -23,9 +29,9 @@ WHERE e.Event_ID IN (
     SELECT e.Event_ID
     FROM events e,
          firstEvent
-    WHERE e.Event_Status <> 'DELETED'
+    WHERE e.Event_Status != 'DELETED'
       AND e.Event_Type NOT IN
-          ('GAME_CREATED', 'GAME_STARTED', 'GAME_FINISHED', 'GAME_GRACE_ONE', 'GAME_GRACE_TWO', 'GAME_PLAYER_LEFT')
+          ('GAME_CREATED', 'GAME_STARTED', 'GAME_FINISHED', 'GAME_GRACE_ONE', 'GAME_GRACE_TWO')
       AND e.Game_ID = firstEvent.Game_ID
       AND e.Player_ID = firstEvent.User_ID
       AND e.Event_ID < firstEvent.Event_ID
