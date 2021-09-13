530a0f53c264d44b833b3133d5929f8f1cc11cc8
Marvin Kreis <kreismar@fim.uni-passau.de>
2021-09-13 22:33:30 +0200

    Update test accordion to use global JS object
    Also refactored the test accordion code a bit.
    Moved all rendering into functions in a subclass.
    Moved popover and modal initialization into DataTables' createdRow callback.

---
+++ src/main/webapp/js/test_accordion.js
@@ -0,0 +1,247 @@
+/* Wrap in a function to avoid polluting the global scope. */
+(function () {
+
+class TestAccordion {
+
+    /**
+     * @param {object[]} categories
+     *      Given by [JSON.parse('${testAccordion.categoriesAsJSON}')].
+     * @param {Map<number, object>} tests
+     *      Given by [new Map(JSON.parse('${testAccordion.testsAsJSON}'))].
+     */
+    constructor (categories, tests) {
+        /** The categories of tests to display. */
+        this.categories = categories;
+
+        /** Maps test ids to the corresponding tests. */
+        this.tests = tests
+
+        /** Maps test ids to modals that show the tests' code. */
+        this.testModals = new Map();
+    }
+
+    /**
+     * Sets up a popover.
+     * @param {HTMLElement} triggerElement A DOM element to be used as the popover trigger.
+     * @param {object} data The data of the row, as given by datatables.
+     * @param {function} renderTitle A function to render the heading of the popover with.
+     * @param {function} renderContent A function to render the body of the popover with.
+     */
+    setupPopover (triggerElement, data, renderTitle, renderContent) {
+        new bootstrap.Popover(triggerElement, {
+            container: document.body,
+            template:
+                `<div class="popover" role="tooltip">
+                    <div class="popover-arrow"></div>
+                    <h3 class="popover-header"></h3>
+                    <div class="popover-body px-3 py-2" style="max-width: 250px;"></div>
+                </div>`,
+            placement: 'top',
+            trigger: 'hover',
+            html: true,
+            title: () => renderTitle(data),
+            content: () => renderContent(data)
+        });
+    };
+
+    /**
+     * Creates a modal to display the given test and shows it.
+     * References to created models are cached in a map so they don't need to be generated again.
+     * @param {object} test The test DTO to display.
+     */
+    viewTestModal (test) {
+        let modal = this.testModals.get(test.id);
+        if (modal !== undefined) {
+            modal.modal('show');
+            return;
+        }
+
+        modal = $(
+                `<div class="modal fade" tabindex="-1" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-responsive">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Test ${test.id} (by ${test.creator.name})</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="card">
+                                    <div class="card-body p-0 codemirror-expand codemirror-test-modal-size">
+                                        <pre class="m-0"><textarea name="test-${test.id}"></textarea></pre>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>`);
+        modal.appendTo(document.body);
+        this.testModals.set(test.id, modal);
+
+        const textarea = modal.find('textarea').get(0);
+        const editor = CodeMirror.fromTextArea(textarea, {
+            lineNumbers: true,
+            matchBrackets: true,
+            mode: "text/x-java",
+            readOnly: true,
+            autoRefresh: true
+        });
+
+        TestAPI.getAndSetEditorValue(textarea, editor);
+        modal.modal('show');
+    };
+
+    init () {
+        /* Bind functions that we need later, since we can't safely use "this" in a callback function. */
+        const setupPopover = this.setupPopover.bind(this);
+        const viewTestModal = this.viewTestModal.bind(this);
+
+        /* Loop through the categories and create a test table for each one. */
+        for (const category of this.categories) {
+            const rows = category.testIds
+                    .sort()
+                    .map(this.tests.get, this.tests);
+
+            /* Create the DataTable. */
+            const tableElement = $('#ta-table-' + category.id);
+            const dataTable = tableElement.DataTable({
+                data: rows,
+                columns: [
+                    { data: null, title: '', defaultContent: '' },
+                    { data: TestAccordion.RenderFunctions.renderId, title: '' },
+                    { data: TestAccordion.RenderFunctions.renderCreator, title: '' },
+                    { data: TestAccordion.RenderFunctions.renderCoveredMutants, title: '' },
+                    { data: TestAccordion.RenderFunctions.renderKilledMutants, title: '' },
+                    { data: TestAccordion.RenderFunctions.renderPoints, title: '' },
+                    { data: TestAccordion.RenderFunctions.renderSmells, title: '' },
+                    { data: TestAccordion.RenderFunctions.renderViewButton, title: '' }
+                ],
+                scrollY: '400px',
+                scrollCollapse: true,
+                paging: false,
+                dom: 't',
+                language: {
+                    emptyTable: category.id === 'all'
+                            ? 'No tests.'
+                            : 'No tests cover this method.'
+                },
+                createdRow: function (row, data, index) {
+                    setupPopover(
+                            row.querySelector('.ta-covered-link'),
+                            data,
+                            TestAccordion.RenderFunctions.renderCoveredMutantsPopoverTitle,
+                            TestAccordion.RenderFunctions.renderCoveredMutantsPopoverBody
+                    );
+
+                    setupPopover(
+                            row.querySelector('.ta-killed-link'),
+                            data,
+                            TestAccordion.RenderFunctions.renderKilledMutantsPopoverTitle,
+                            TestAccordion.RenderFunctions.renderKilledMutantsPopoverBody
+                    );
+
+                    setupPopover(
+                            row.querySelector('.ta-smells-link'),
+                            data,
+                            TestAccordion.RenderFunctions.renderSmellsPopoverTitle,
+                            TestAccordion.RenderFunctions.renderSmellsPopoverBody
+                    );
+
+                    row.querySelector('.ta-view-button').addEventListener('click', function (event) {
+                        viewTestModal(data);
+                    });
+                }
+            });
+        }
+    }
+
+    /**
+     * Functions to render the table content from data.
+     */
+    static RenderFunctions = class RenderFunctions {
+        static renderId (data) {
+            return `Test ${data.id}`;
+        }
+
+        static renderCreator (data) {
+            return data.creator.name;
+        }
+
+        static renderPoints (data) {
+            return `<span class="ta-column-name">Points:</span> ${data.points}`;
+        }
+
+        static renderCoveredMutants (data) {
+            return `<span class="ta-covered-link"><span class="ta-column-name">Covered:</span> ${data.coveredMutantIds.length}</span>`;
+        }
+
+        static renderKilledMutants (data) {
+            return `<span class="ta-killed-link"><span class="ta-column-name">Killed:</span> ${data.killedMutantIds.length}</span>`;
+        }
+
+        static renderViewButton (data) {
+            return data.canView
+                    ? '<button class="ta-view-button btn btn-xs btn-primary">View</button>'
+                    : '';
+        }
+
+        static renderSmells (data) {
+            const numSmells = data.smells.length;
+            let smellLevel;
+            let smellColor;
+            if (numSmells >= 3) {
+                smellLevel = 'Bad';
+                smellColor = 'btn-danger';
+            } else if (numSmells >= 1) {
+                smellLevel = 'Fishy';
+                smellColor = 'btn-warning';
+            } else {
+                smellLevel = 'Good';
+                smellColor = 'btn-success';
+            }
+            return `<a class="ta-smells-link btn btn-xs ${smellColor}">${smellLevel}</a>`;
+        }
+
+        static renderCoveredMutantsPopoverTitle (data) {
+            return data.coveredMutantIds.length > 0
+                    ? 'Covered Mutants'
+                    : '';
+        }
+
+        static renderCoveredMutantsPopoverBody (data) {
+            return data.coveredMutantIds.length > 0
+                    ? data.coveredMutantIds.join(', ')
+                    : 'No mutants are covered by this test.';
+        }
+
+        static renderKilledMutantsPopoverTitle (data) {
+            return data.killedMutantIds.length > 0
+                    ? 'Killed Mutants'
+                    : '';
+        }
+
+        static renderKilledMutantsPopoverBody (data) {
+            return data.killedMutantIds.length > 0
+                    ? data.killedMutantIds.join(', ')
+                    : 'No mutants were killed by this test.';
+        }
+
+        static renderSmellsPopoverTitle (data) {
+            return data.smells.length > 0
+                    ? 'Test Smells'
+                    : '';
+        }
+
+        static renderSmellsPopoverBody (data) {
+            return data.smells.length > 0
+                    ? data.smells.join('<br>')
+                    : 'This test does not have any smells.'
+        }
+    };
+}
+
+CodeDefenders.classes.TestAccordion = TestAccordion;
+
+})();
