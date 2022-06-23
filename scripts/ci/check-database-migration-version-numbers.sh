1b5a9e5415ca49dd7d32cac63e1575e36bb336e9
Alexander Degenhart <degenhart@fim.uni-passau.de>
2022-06-23 13:28:59 +0200

    Enable running Java-based database migrations.

--- scripts/ci/check-database-migration-version-numbers.sh
+++ scripts/ci/check-database-migration-version-numbers.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-output="$(find src/main/resources/db -name "V*" | sed -e 's/.*\(V[0-9._]*\)__.*/\1/' | sort | uniq -d)"
+# This matches SQL migrations (in src/main/resources/db/migrations/)
+# and Java migrations (in src/java/org/codedefenders/persistence/database/migrations)
+output="$(find src/main/ -path "*/migrations/V*__*.*" | sed -e 's/.*\(V[0-9._]*\)__.*/\1/' | sort | uniq -d)"
 if [ -z "$output" ]; then
     echo "No duplicate versions"
     exit 0
