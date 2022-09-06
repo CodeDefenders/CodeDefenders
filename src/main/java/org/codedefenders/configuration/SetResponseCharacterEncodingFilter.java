bc2bff11fa1d82a5384855dc12c0ebdf42010ff5
Alexander Degenhart <degenhart@fim.uni-passau.de>
2022-09-06 10:38:47 +0200

    Refactor/order Servlet Filter declaration in web.xml

--- src/main/java/org/codedefenders/configuration/SetResponseCharacterEncodingFilter.java
+++ src/main/java/org/codedefenders/configuration/SetResponseCharacterEncodingFilter.java
@@ -7,10 +7,12 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 
 /**
  * Set response encoding to UTF-8.
  */
+@WebFilter(filterName = "ResponseCharacterEncoding")
 public class SetResponseCharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
