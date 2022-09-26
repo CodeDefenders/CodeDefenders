ce04bb55821bf791e912f70e6a5350fed63e621f
Alexander Degenhart <degenhart@fim.uni-passau.de>
2022-09-26 13:43:42 +0200

    Replace deprecated org.junit.Assert.assertThat() with org.hamcrest.MatcherAssert.assertThat()

--- src/test/java/org/codedefenders/beans/admin/StagedGameListTest.java
+++ src/test/java/org/codedefenders/beans/admin/StagedGameListTest.java
@@ -5,6 +5,7 @@ import org.codedefenders.beans.admin.StagedGameList.StagedGame;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
@@ -12,7 +13,6 @@ import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 public class StagedGameListTest {
