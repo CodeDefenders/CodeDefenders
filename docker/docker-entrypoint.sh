082163f317bdec37903dfcf66cfbb54a79b7792d
Alexander Degenhart <degenhart@fim.uni-passau.de>
2022-10-29 12:21:01 +0200

    Update docker-entrypoint and include script for building a (reproducible) container image

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,18 +3,19 @@
 gen_tomcat_users() {
     FILE="$1"
     ADMIN_USERNAME="$2"
+    ADMIN_ROLE="$3"
 
-    echo '<?xml version="1.0" encoding="UTF-8"?>' > "$FILE"
+    echo '<?xml version="1.0" encoding="UTF-8"?>' > "${FILE}"
     echo '<tomcat-users xmlns="http://tomcat.apache.org/xml"
               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
               xsi:schemaLocation="http://tomcat.apache.org/xml tomcat-users.xsd"
-              version="1.0">' >> "$FILE"
-    if [ -n "$ADMIN_USERNAME" ]
+              version="1.0">' >> "${FILE}"
+    if [ -n "${ADMIN_USERNAME}" ]
     then
-        echo '<role rolename="codedefenders-admin"/>' >> "$FILE"
-        echo "<user username=\"$ADMIN_USERNAME\" roles=\"codedefenders-admin\"/>" >> "$FILE"
+        echo "<role rolename=\"${ADMIN_ROLE}\"/>" >> "${FILE}"
+        echo "<user username=\"${ADMIN_USERNAME}\" roles=\"${ADMIN_ROLE}\"/>" >> "${FILE}"
     fi
-    echo '</tomcat-users>' >> "$FILE"
+    echo '</tomcat-users>' >> "${FILE}"
 }
 
 # TODO: Remove if execution dependency management is handled by CodeDefenders (GitLab #746)
@@ -27,7 +28,7 @@ download_dependencies() {
 }
 
 main() {
-    gen_tomcat_users "/etc/tomcat9/tomcat-users.xml" "$CODEDEFENDERS_ADMIN_USERNAME"
+    gen_tomcat_users "/etc/tomcat9/tomcat-users.xml" "${CODEDEFENDERS_ADMIN_USERNAME}" "${CODEDEFENDERS_AUTH_ADMIN_ROLE:-"codedefenders-admin"}"
     download_dependencies "/srv/codedefenders"
 }
 
