376795b08504cd4a636a0c9809a750f4c6dda993
Alexander Degenhart <degenhart@fim.uni-passau.de>
2022-12-12 12:40:46 +0100

    Remove unused test resources

--- src/test/resources/EmptyTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.*;
-import static org.junit.Assert.*;
-
-public class EmptyTest {
-	@Test(timeout = 4000)
-	public void test() throws Throwable {
-		// nothing here
-	}
-}
--- src/test/resources/TestWithSystemCall.java
+++
@@ -1,9 +0,0 @@
-import org.junit.*;
-import static org.junit.Assert.*;
-
-public class TestWithSystemCall {
-	@Test(timeout = 4000)
-	public void test() throws Throwable {
-		System.out.println("Hello world");
-	}
-}
