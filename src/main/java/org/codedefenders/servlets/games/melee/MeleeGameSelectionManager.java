CodeDefenders/CodeDefenders

commit bd7a0455766f89eb346301552bc17b1724153c28

Merge MeleeGame::addPlayer methods. Fixes #978

src/main/java/org/codedefenders/servlets/games/melee/MeleeGameSelectionManager.java
@@ -229,7 +229,7 @@ public class MeleeGameSelectionManager extends HttpServlet {
             response.sendRedirect(url.forPath(Paths.MELEE_GAME) + "?gameId=" + gameId);
             return;
         }
-        if (game.addPlayer(login.getUserId())) {
+        if (game.addPlayer(login.getUserId(), Role.PLAYER)) {
             logger.info("User {} joined game {}.", login.getUserId(), gameId);
 
             /*

src/main/java/org/codedefenders/game/multiplayer/MeleeGame.java
@@ -345,23 +345,6 @@ public class MeleeGame extends AbstractGame {
         return !requiresValidation || userRepository.getUserById(userId).map(UserEntity::isValidated).orElse(false);
     }
 
-    public boolean addPlayer(int userId) {
-        if (canJoinGame(userId) && addPlayerForce(userId, Role.PLAYER)) {
-            Optional<UserEntity> u = userRepository.getUserById(userId);
-            final Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-            Event e = new Event(-1, id, userId, u.map(UserEntity::getUsername).orElse("") + " joined melee game", EventType.PLAYER_JOINED,
-                    EventStatus.GAME, timestamp);
-            eventDAO.insert(e);
-            Event notif = new Event(-1, id, userId, "You joined melee game", EventType.PLAYER_JOINED, EventStatus.NEW,
-                    timestamp);
-            eventDAO.insert(notif);
-            //
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     @Override
     public boolean addPlayer(int userId, Role role) {
         return canJoinGame(userId) && addPlayerForce(userId, role);
@@ -375,6 +358,14 @@ public class MeleeGame extends AbstractGame {
         if (!GameDAO.addPlayerToGame(id, userId, role)) {
             return false;
         }
+        Optional<UserEntity> u = userRepository.getUserById(userId);
+        final Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        Event e = new Event(-1, id, userId, u.map(UserEntity::getUsername).orElse("") + " joined melee game", EventType.PLAYER_JOINED,
+                EventStatus.GAME, timestamp);
+        eventDAO.insert(e);
+        Event notif = new Event(-1, id, userId, "You joined melee game", EventType.PLAYER_JOINED, EventStatus.NEW,
+                timestamp);
+        eventDAO.insert(notif);
 
         return true;
     }

commit f9552864e2c0d18f4bb57b77737a24a47e807974

Fix 500 error on creating MeleeGames
This is a regression introduced in bd7a0455.

src/main/java/org/codedefenders/servlets/games/melee/MeleeGameSelectionManager.java
@@ -51,6 +51,7 @@ import org.codedefenders.notification.events.server.game.GameCreatedEvent;
 import org.codedefenders.notification.events.server.game.GameJoinedEvent;
 import org.codedefenders.notification.events.server.game.GameLeftEvent;
 import org.codedefenders.notification.events.server.game.GameStartedEvent;
+import org.codedefenders.persistence.database.UserRepository;
 import org.codedefenders.service.game.GameService;
 import org.codedefenders.servlets.admin.AdminSystemSettings;
 import org.codedefenders.servlets.games.GameManagingUtils;
@@ -97,6 +98,9 @@ public class MeleeGameSelectionManager extends HttpServlet {
 
     @Inject
     private EventDAO eventDAO;
+
+    @Inject
+    private UserRepository userRepo;
 
     @Inject
     private GameManagingUtils gameManagingUtils;
@@ -358,6 +362,9 @@ public class MeleeGameSelectionManager extends HttpServlet {
             messages.add("Creating games is currently not enabled.");
             return false;
         }
+
+        game.setEventDAO(eventDAO);
+        game.setUserRepository(userRepo);
 
         int newGameId = MeleeGameDAO.storeMeleeGame(game);
         game.setId(newGameId);