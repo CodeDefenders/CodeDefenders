57e0f66ae58fae95697b9743240ac63980467856
Marvin Kreis <kreismar@fim.uni-passau.de>
2023-06-16 17:48:35 +0200

    Update classrooms table
    - Replace room code with UUID
    - Add Visible and Archived columns

--- src/main/resources/db/migrations/V17__add_classrooms.sql
+++ src/main/resources/db/migrations/V17__add_classrooms.sql
@@ -1,19 +1,23 @@
 CREATE TABLE classrooms
 (
-    ID        int(11)      NOT NULL AUTO_INCREMENT,
-    Name      VARCHAR(100) NOT NULL,
-    Room_Code VARCHAR(20)  NOT NULL,
-    Password  CHAR(60)     NULL DEFAULT NULL,
-    Open      tinyint(1)   NOT NULL DEFAULT 0,
+    ID         int(11)      NOT NULL AUTO_INCREMENT,
+    UUID       UUID         NOT NULL,
+    Creator_ID int(11)      NULL,
+    Name       VARCHAR(100) NOT NULL,
+    Password   CHAR(60)     NULL DEFAULT NULL,
+    Open       tinyint(1)   NOT NULL DEFAULT 0,
+    Visible    tinyint(1)   NOT NULL DEFAULT 0,
+    Archived   tinyint(1)   NOT NULL DEFAULT 0,
 
     PRIMARY KEY (ID),
 
-    CONSTRAINT unique_room_code
-        UNIQUE (Room_Code),
+    CONSTRAINT unique_uuid UNIQUE (UUID),
 
     CONSTRAINT check_name CHECK (Name <> ''),
     CONSTRAINT check_password CHECK (Password <> ''),
-    CONSTRAINT check_room_code CHECK (Room_Code <> '')
+
+    CONSTRAINT fk_creator_id
+        FOREIGN KEY (Creator_ID) REFERENCES users (User_ID) ON UPDATE CASCADE ON DELETE SET NULL
 )
 AUTO_INCREMENT = 100;
 
