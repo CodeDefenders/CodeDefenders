a688c754edfac240266ef47a6e5be117141b8618
Tim Greller <timlg07@gmail.com>
2023-08-20 23:59:31 +0200

    achievements: add level 0 to db (and enforce foreign key)

--- src/main/resources/db/migrations/V17__add_achievements.sql
+++ src/main/resources/db/migrations/V17__add_achievements.sql
@@ -18,36 +18,42 @@ CREATE TABLE has_achievement
     `User_ID`            INT REFERENCES `users` (`User_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
     `Notification_Shown` BOOL DEFAULT FALSE,
     `Metric`            INT NOT NULL,
-    PRIMARY KEY (`Achievement_ID`, `User_ID`)
-    #, CONSTRAINT `achievement_user` FOREIGN KEY (`Achievement_ID`, `Achievement_level`) REFERENCES `achievements` (`ID`, `level`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    PRIMARY KEY (`Achievement_ID`, `User_ID`),
+    CONSTRAINT `achievement_user` FOREIGN KEY (`Achievement_ID`, `Achievement_level`) REFERENCES `achievements` (`ID`, `level`) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
 INSERT INTO achievements
-VALUES (0, 1, 'Newbie', 'Play your first game!', 1),
+VALUES (0, 0, 'No games played yet', 'Play your first game to unlock this achievement', 0),
+       (0, 1, 'Newbie', 'Play your first game!', 1),
        (0, 2, 'Bronze Player', 'Play {0} games', 3),
        (0, 3, 'Silver Player', 'Play {0} games', 10),
        (0, 4, 'Gold Player', 'Play {0} games', 50),
 
+       (1, 0, 'No game played as attacker yet', 'Play as attacker to unlock this achievement', 0),
        (1, 1, 'Prepare to Attack', 'Play your first multiplayer game as attacker', 1),
        (1, 2, 'Bronze Attacker', 'Play {0} games as attacker', 3),
        (1, 3, 'Silver Attacker', 'Play {0} games as attacker', 10),
        (1, 4, 'Gold Attacker', 'Play {0} games as attacker', 50),
 
+       (2, 0, 'No game played as defender yet', 'Play as defender to unlock this achievement', 0),
        (2, 1, 'Prepare Your Defenses', 'Play your first multiplayer game as defender', 1),
        (2, 2, 'Bronze Defender', 'Play {0} games as defender', 3),
        (2, 3, 'Silver Defender', 'Play {0} games as defender', 10),
        (2, 4, 'Gold Defender', 'Play {0} games as defender', 50),
 
+       (3, 0, 'No melee game played yet', 'Try the melee mode to unlock this achievement', 0),
        (3, 1, 'Melee Starter', 'Play your first melee game', 1),
        (3, 2, 'Melee Bronze', 'Play {0} melee games', 3),
        (3, 3, 'Melee Silver', 'Play {0} melee games', 10),
        (3, 4, 'Melee Gold', 'Play {0} melee games', 50),
 
+       (4, 0, 'No tests written yet', 'Write tests to unlock this achievement', 0),
        (4, 1, 'The First Test', 'Write your first test', 1),
        (4, 2, 'Bronze Test Writer', 'Write {0} tests', 10),
        (4, 3, 'Silver Test Writer', 'Write {0} tests', 50),
        (4, 4, 'Gold Test Writer', 'Write {0} tests', 200),
 
+       (5, 0, 'No mutants created yet', 'Create mutants to unlock this achievement', 0),
        (5, 1, 'The First Mutant', 'Create your first mutant', 1),
        (5, 2, 'Bronze Mutant Creator', 'Create {0} mutants', 10),
        (5, 3, 'Silver Mutant Creator', 'Create {0} mutants', 50),
