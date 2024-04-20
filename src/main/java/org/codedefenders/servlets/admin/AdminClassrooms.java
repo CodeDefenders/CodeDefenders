90814624f002248cc1f69adb85848b8b430d0b90
Marvin Kreis <kreismar@fim.uni-passau.de>
2024-04-20 19:00:27 +0200

    Add admin classrooms servlet again
    Whoops, I forgot that it was still used for GET requests.

---
+++ src/main/java/org/codedefenders/servlets/admin/AdminClassrooms.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016-2019 Code Defenders contributors
+ *
+ * This file is part of Code Defenders.
+ *
+ * Code Defenders is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Code Defenders is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Code Defenders. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.codedefenders.servlets.admin;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.ValidationException;
+
+import org.codedefenders.auth.CodeDefendersAuth;
+import org.codedefenders.beans.message.MessagesBean;
+import org.codedefenders.service.ClassroomService;
+import org.codedefenders.servlets.util.Redirect;
+import org.codedefenders.servlets.util.ServletUtils;
+import org.codedefenders.util.Paths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet(Paths.ADMIN_CLASSROOMS)
+public class AdminClassrooms extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/jsp/admin_classrooms.jsp").forward(request, response);
+    }
+}
