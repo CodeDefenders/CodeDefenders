ce0651c395213e6d02b0de70918626d6e5d7d80e
Marvin Kreis <kreismar@fim.uni-passau.de>
2024-04-23 21:25:15 +0200

    Add system role and subject

---
+++ src/main/java/org/codedefenders/auth/roles/SystemRole.java
@@ -0,0 +1,16 @@
+package org.codedefenders.auth.roles;
+
+import java.util.Set;
+
+import org.apache.shiro.authz.permission.AllPermission;
+
+/**
+ * A role for system tasks. E.g. cron jobs or startup tasks.
+ */
+public class SystemRole extends Role {
+    public static final String name = "system";
+
+    public SystemRole() {
+        super(name, Set.of(new AllPermission()));
+    }
+}
