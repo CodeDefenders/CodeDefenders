3775207a478ebc612e2c27586bf19106e439edd7
Marvin Kreis <kreismar@fim.uni-passau.de>
2024-08-08 12:05:31 +0200

    Convert puzzles and add new shell script for packing

---
+++ installation/puzzles/Chapter 02 - Intermediate/Puzzle 08/tests/01/TestPuzzle.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class TestPuzzle {
+
+    @Test(timeout = 4000)
+    public void test() {
+        Puzzle b = new Puzzle();
+        assertEquals(-1, b.run(0, 0));
+    }
+
+}
+
---
+++ installation/puzzles/pack_puzzles.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+shopt -s extglob
+
+if [[ "$(basename "$PWD")" != "puzzles" ]]; then
+    >&2 echo This script must be executed from within the puzzle directory.
+    exit 1
+fi
+
+for chapter in !(*.zip|*.sh); do (
+    cd "$chapter"
+
+    rm -f "../${chapter}.zip"
+    zip -r "../${chapter}" !(*.zip)
+
+    for puzzle in !(*.zip|*.properties); do (
+        cd "$puzzle"
+
+        rm -f "../${puzzle}.zip"
+        zip -r "../${puzzle}" *
+    ) done
+) done
