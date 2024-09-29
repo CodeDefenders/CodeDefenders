0c3b9759a0ba333649c7ab62d9b729260f1a6c7e
Tim Greller <timlg07@gmail.com>
2024-09-30 00:42:26 +0200

    fix cut for equiv puzzle 3

--- installation/puzzles/mutants/Equiv3/0/Puzzle.java
+++ installation/puzzles/mutants/Equiv3/0/Puzzle.java
@@ -3,7 +3,7 @@ public class Puzzle {
     public String run(int x) {
         String result = "";
         for (int i = 1; i <= x; i++) {
-            if (i % 10 == 0) {
+            if ((i + 1) % 10 == 0) {
                 result += "X";
             } else {
                 result += "O";
