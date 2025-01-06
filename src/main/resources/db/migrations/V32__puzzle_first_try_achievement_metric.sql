902940d6eea7057291f8a65689c05d2c10d153f3
Tim Greller <timlg07@gmail.com>
2025-01-06 18:05:06 +0100

    do not use view in db migration

--- src/main/resources/db/migrations/V32__puzzle_first_try_achievement_metric.sql
+++ src/main/resources/db/migrations/V32__puzzle_first_try_achievement_metric.sql
@@ -1,8 +1,9 @@
 # Update metrics for solved puzzles
 INSERT INTO has_achievement (Achievement_ID, User_ID, Metric)
 SELECT 19, Creator_ID as user, count(*) as SolvedPuzzles
-FROM view_puzzle_games
-WHERE State = 'SOLVED'
+FROM games
+WHERE Mode = 'PUZZLE'
+  AND State = 'SOLVED'
   AND CurrentRound = 1
 GROUP BY Creator_ID
 ON DUPLICATE KEY UPDATE Metric = VALUES(Metric);
