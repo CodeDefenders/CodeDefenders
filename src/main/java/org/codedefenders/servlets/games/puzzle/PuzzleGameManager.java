d7fe3756d4cd3526da6643dea0367116c72b5d28
Tim Greller <timlg07@gmail.com>
2025-03-21 17:51:01 +0100

    PuzzleGameManager: Use format strings instead of concatenation.

--- src/main/java/org/codedefenders/servlets/games/puzzle/PuzzleGameManager.java
+++ src/main/java/org/codedefenders/servlets/games/puzzle/PuzzleGameManager.java
@@ -20,6 +20,7 @@ package org.codedefenders.servlets.games.puzzle;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Optional;
 
@@ -973,26 +974,28 @@ public class PuzzleGameManager extends HttpServlet {
                             || (playedGame.getState() != GameState.SOLVED) // played but not yet solved.
                     ) {
                         if (puzzleChapter.getChapterId() > currentChapter) {
-                            message.append("<br>")
-                                    .append("You solved all the puzzles of the current chapter! ")
-                                    .append("Start with the <a href=\"")
-                                    .append(url.forPath(Paths.PUZZLE_GAME))
-                                    .append("?puzzleId=")
-                                    .append(puzzle.getPuzzleId())
-                                    .append("\">first puzzle</a> of the next chapter \"")
-                                    .append(puzzleChapter.getTitle())
-                                    .append("\", or go back to the <a href=\"")
-                                    .append(url.forPath(Paths.PUZZLE_GAME))
-                                    .append("\">Puzzle Overview</a>.");
+                            message.append(MessageFormat.format("""
+                                                <br>
+                                                You solved all the puzzles of the current chapter!
+                                                Start with the
+                                                <a href="{0}?puzzleId={1,number,#}">first puzzle</a>
+                                                of the next chapter "{2}", or go back to the
+                                                <a href="{0}">Puzzle Overview</a>.
+                                            """.stripIndent(),
+                                    url.forPath(Paths.PUZZLE_GAME),
+                                    puzzle.getPuzzleId(),
+                                    puzzleChapter.getTitle()
+                            ));
                         } else {
-                            message.append(" ")
-                                    .append("Try to solve the <a href=\"")
-                                    .append(url.forPath(Paths.PUZZLE_GAME))
-                                    .append("?puzzleId=")
-                                    .append(puzzle.getPuzzleId())
-                                    .append("\">next Puzzle</a>, or go back to the <a href=\"")
-                                    .append(url.forPath(Paths.PUZZLE_GAME))
-                                    .append("\">Puzzle Overview</a>.");
+                            message.append(MessageFormat.format("""
+                                                Try to solve the
+                                                <a href="{0}?puzzleId={1,number,#}">next Puzzle</a>,
+                                                or go back to the
+                                                <a href="{0}">Puzzle Overview</a>.
+                                            """,
+                                    url.forPath(Paths.PUZZLE_GAME),
+                                    puzzle.getPuzzleId()
+                            ));
                         }
                         return message.toString();
                     }
