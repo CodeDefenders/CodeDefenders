7d2bba91e7017b30889e954965a6ade5cbbf6768
Tim Greller <timlg07@gmail.com>
2025-04-23 23:39:15 +0200

    Move mutant resolution to gameService

--- src/main/java/org/codedefenders/service/game/AbstractGameService.java
+++ src/main/java/org/codedefenders/service/game/AbstractGameService.java
@@ -71,6 +71,9 @@ public abstract class AbstractGameService implements IGameService {
     protected TestSmellRepository testSmellRepo;
 
     @Inject
+    private INotificationService notificationService;
+
+    @Inject
     public AbstractGameService(GameManagingUtils gameManagingUtils, UserService userService,
                                UserRepository userRepository,
                                TestRepository testRepo, MutantRepository mutantRepo, GameRepository gameRepo,
@@ -84,9 +87,6 @@ public abstract class AbstractGameService implements IGameService {
         this.playerRepo = playerRepo;
         this.testSmellRepo = testSmellRepo;
     }
-
-    @Inject
-    private INotificationService notificationService;
 
     @Override
     public MutantDTO getMutant(int userId, int mutantId) {
@@ -269,8 +269,6 @@ public abstract class AbstractGameService implements IGameService {
     // Todo: only receive the game-Id as parameter and do not update the whole game
     @Override
     public boolean closeGame(AbstractGame game) {
-        resolveAllOpenDuels(game);
-
         game.setState(GameState.FINISHED);
         boolean updated = game.update();
 
@@ -281,32 +279,6 @@ public abstract class AbstractGameService implements IGameService {
         }
 
         return updated;
-    }
-
-    private void resolveAllOpenDuels(AbstractGame game) {
-        List<Mutant> mutantsPendingTests = game.getMutantsMarkedEquivalentPending();
-        for (Mutant mutant : mutantsPendingTests) {
-            boolean isKillable = gameManagingUtils.isMutantKillableByOtherTests(mutant);
-            /*
-             * Points are handled by MultiplayerGame.java:456, a new equivalence status would be needed to change the scoring.
-             * As the scoring rules are already hard to grasp for new players, this would introduce additional complexity.
-             *
-             * Additionally, the attacker's possibility to gain points from the auto-resolved duel stops the defenders
-             * from claiming all mutants as equivalent before the game ends without any risk.
-             */
-            if (isKillable) {
-                mutantRepo.killMutant(mutant, Mutant.Equivalence.PROVEN_NO);
-            } else {
-                mutantRepo.killMutant(mutant, Mutant.Equivalence.ASSUMED_YES);
-                int playerIdDefender = mutantRepo.getEquivalentDefenderId(mutant);
-                playerRepo.increasePlayerPoints(1, playerIdDefender);
-            }
-
-            logger.info("Mutant {} was automatically resolved as {}.",
-                    mutant.getId(), isKillable ? "not equivalent" : "equivalent");
-
-            // TODO: create events like the EquivalenceDuelAttackerWonEvent?
-        }
     }
 
     // Todo: only receive the game-Id as parameter and do not update the whole game
