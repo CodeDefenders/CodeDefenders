4dabc0a089d6becfcd3d5077e9d81cdb34d848a0
Aaron <prott@fim.uni-passau.de>
2025-04-24 14:57:06 +0200

    Add table for invitation codes

--- src/main/resources/db/migrations/V36__invitations.sql
+++ src/main/resources/db/migrations/V36__invitations.sql
@@ -7,3 +7,10 @@ create table if not exists whitelist(
 alter table games
     add column if not exists invite_only bool default false,
     add column if not exists may_choose_role bool default true;
+
+create table if not exists invitation_links
+(
+    invitation_id int(11) not null primary key auto_increment,
+    game_id       int(11),
+    foreign key (game_id) references games (ID) on delete cascade
+);
