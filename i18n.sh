ecf79936ce04751fc79a33e01bc31331606517f0
Tim Greller <timlg07@gmail.com>
2025-07-22 23:31:16 +0200

    Shell script for translation extraction & compilation.

--- i18n.sh
+++ i18n.sh
@@ -1,14 +1,12 @@
-# download and install the latest gettext version from https://launchpad.net/ubuntu/+source/gettext
-# tested with 0.23.1 (from 2025-05-29)
-
-# install the xgettext-regex tool via npm:
-npm i xgettext-regex --global
-# TODO: add as dev dependency to package.json?
-
 # step 1: create keys.pot file by extracting all strings that should be translated
-xgettext-regex src -f i18n.tr -o po/keys.pot
-# step 2: use PoEdit to create .po files containing the translations for all languages
-# step 3: create the Messages_language.class resource bundle files using the .po files
-msgfmt --java2 -d target/classes -r org.codedefenders.i18n.Messages -l de po/de.po
+if [ "$1" = "extract" ]; then
+  node ./node_modules/xgettext-regex/bin/xgettext-regex.js src -f i18n.tr -o po/keys.pot
+fi
 
-# more information: https://xnap-commons.sourceforge.net/gettext-commons/tutorial.html#Creating_Resource_Bundles
+# step 3: create the Messages_language.class resource bundle files using the .po files
+if [ "$1" = "compile" ]; then
+  for po_file in po/*.po; do
+    lang=$(basename "$po_file" .po)
+    msgfmt --java2 -d target/classes -r org.codedefenders.i18n.Messages -l "$lang" "$po_file"
+  done
+fi
