e7acaf8858ead321dae3baeeecaedbe8a783482a
Tim Greller <timlg07@gmail.com>
2025-07-22 23:36:56 +0200

    improve i18n.sh: complain about an invalid parameter

--- i18n.sh
+++ i18n.sh
@@ -1,12 +1,14 @@
-# step 1: create keys.pot file by extracting all strings that should be translated
 if [ "$1" = "extract" ]; then
+  # create keys.pot file by extracting all strings that should be translated
   node ./node_modules/xgettext-regex/bin/xgettext-regex.js src -f i18n.tr -o po/keys.pot
-fi
-
-# step 3: create the Messages_language.class resource bundle files using the .po files
-if [ "$1" = "compile" ]; then
+elif [ "$1" = "compile" ]; then
+  # create the Messages_language.class resource bundle files for all of the .po files
   for po_file in po/*.po; do
     lang=$(basename "$po_file" .po)
     msgfmt --java2 -d target/classes -r org.codedefenders.i18n.Messages -l "$lang" "$po_file"
   done
+else
+  echo "Unknown parameter: '$1'"
+  echo "Usage: $0 [extract|compile]"
+  exit 1
 fi
