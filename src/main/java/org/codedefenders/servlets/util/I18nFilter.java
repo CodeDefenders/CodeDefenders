35fe760e9ff70e34c6673fdb838309802577e8bd
Tim Greller <timlg07@gmail.com>
2025-07-22 19:52:53 +0200

    implement a web filter to provide an i18n instance in every jsp servlet

---
+++ src/main/java/org/codedefenders/servlets/util/I18nFilter.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2016-2025 Code Defenders contributors
+ *
+ * This file is part of Code Defenders.
+ *
+ * Code Defenders is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Code Defenders is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Code Defenders. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.codedefenders.servlets.util;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+
+import org.codedefenders.util.I18nUtils;
+import org.xnap.commons.i18n.I18n;
+
+@WebFilter("/*")
+public class I18nFilter implements Filter {
+
+    private static final String i18nAttrName = "i18n";
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        if (request.getAttribute(i18nAttrName) == null) {
+            I18n i18n = I18nUtils.getI18n(request.getLocale());
+            request.setAttribute(i18nAttrName, i18n);
+        }
+
+        chain.doFilter(request, response);
+    }
+}
+
