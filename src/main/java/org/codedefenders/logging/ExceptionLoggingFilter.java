a9e085c43f36b1d3f5780fc15611e4e4b78dfa67
Marvin Kreis <kreismar@fim.uni-passau.de>
2026-04-08 17:39:12 +0200

    Add filter for logging exceptions ourselves

---
+++ src/main/java/org/codedefenders/logging/ExceptionLoggingFilter.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2016-2025 Code Defenders contributors
+ *
+ * This file is part of Code Defenders.
+ *
+ * Code Defenders is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Code Defenders is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Code Defenders. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.codedefenders.logging;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class ExceptionLoggingFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionLoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Throwable e) {
+            logger.error("Unhandled exception", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
