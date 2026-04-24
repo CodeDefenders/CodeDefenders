7b68ebbeb6547eded36dd00868d79a6a63689286
Aaron <prott@fim.uni-passau.de>
2026-04-24 11:21:16 +0200

    Migration doesn't crash if AI users already exist

--- src/main/resources/db/migrations/V37__add_ai_players.sql
+++ src/main/resources/db/migrations/V37__add_ai_players.sql
@@ -1,4 +1,4 @@
-insert into `users` (`User_ID`, `Username`, `Password`, `Email`)
+insert ignore into `users` (`User_ID`, `Username`, `Password`, `Email`)
 values (5, 'AI_Attacker', 'AI_ATTACKER_INACCESSIBLE', 'aiattacker@dummy.com'),
        (6, 'AI_Defender', 'AI_DEFENDER_INACCESSIBLE', 'aidefender@dummy.com'),
        (7, 'AI_PLAYER', 'AI_PLAYER_INACCESSIBLE', 'aiplayer@dummy.com');
